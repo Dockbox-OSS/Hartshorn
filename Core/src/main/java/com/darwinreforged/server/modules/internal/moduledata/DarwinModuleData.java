41acf178612bd40a22471882634dbc1af403dba4
Guus Lieben <guus@xendox.com>
2020-05-03 23:19:48 +0200

    Add module structure to core

---
+++ Core/src/main/java/com/darwinreforged/server/modules/internal/moduledata/DarwinModuleData.java
@@ -0,0 +1,66 @@
+package com.darwinreforged.server.modules.internal.moduledata;
+
+import com.darwinreforged.server.core.events.ServerInitEvent;
+import com.darwinreforged.server.core.events.ServerStartedEvent;
+import com.darwinreforged.server.core.events.util.Listener;
+import com.darwinreforged.server.core.init.DarwinServer;
+import com.darwinreforged.server.core.modules.ModuleInfo;
+import com.darwinreforged.server.core.modules.PluginModule;
+import com.darwinreforged.server.core.util.FileUtils;
+
+import org.yaml.snakeyaml.Yaml;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+@ModuleInfo(id = "darwinmoduledata", name = "Sponge Plugin Data", version = "0.0.2", description = "Collects and stores plugin data", authors = {"DiggyNevs"})
+public class DarwinModuleData extends PluginModule {
+
+    private final Map<String, Map<String, Object>> data = new HashMap<>();
+    private static File dataFile;
+
+    @Listener
+    public void onServerStart(ServerStartedEvent event) {
+        dataFile = new File(DarwinServer.getUtilChecked(FileUtils.class).getDataDirectory(this).toFile(), "plugin_data.yml");
+        if (!dataFile.exists()) {
+            try {
+                dataFile.getParentFile().mkdirs();
+                dataFile.createNewFile();
+            } catch (IOException e) {
+                System.err.println(e.getMessage());
+            }
+        }
+    }
+
+    @Listener
+    public void onServerFinishLoad(ServerInitEvent event) {
+        DarwinServer.getAllModuleInfo().forEach(this::registerPlugin);
+        try {
+            Yaml yaml = new Yaml();
+            FileWriter writer = new FileWriter(dataFile);
+            yaml.dump(data, writer);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void registerPlugin(ModuleInfo module) {
+        // Data storage before Yaml conversion
+        Map<String, Object> data = new HashMap<>();
+
+        // Generic data
+        data.put("id", module.id());
+        data.put("name", module.name());
+        data.put("version", module.version());
+        data.put("description", module.description());
+        data.put("url", module.url());
+        data.put("authors", module.authors());
+        data.put("source", module.source());
+
+        // Write plugin data to unique file
+        this.data.put(module.id(), data);
+    }
+}
