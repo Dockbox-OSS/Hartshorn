668a2f90e26827b208ab5173e95f9f7630e13d74
Guus Lieben <guus@xendox.com>
2020-05-04 21:55:33 +0200

    Remove now absolete interface methods

--- Core/src/main/java/com/darwinreforged/server/core/modules/PluginModule.java
+++ Core/src/main/java/com/darwinreforged/server/core/modules/PluginModule.java
@@ -1,22 +1,6 @@
 package com.darwinreforged.server.core.modules;
 
-import com.darwinreforged.server.core.events.internal.ServerInitEvent;
-import com.darwinreforged.server.core.events.internal.ServerReloadEvent;
-import com.darwinreforged.server.core.events.internal.ServerStartedEvent;
-
 public abstract class PluginModule
         implements PluginModuleNative {
-    // Doesn't force modules to have these events implemented by default
-    @Override
-    public void onServerFinishLoad(ServerInitEvent event) {
-    }
-
-    @Override
-    public void onServerStart(ServerStartedEvent event) {
-    }
-
-    @Override
-    public void onServerReload(ServerReloadEvent event) {
-    }
 }
 
--- Core/src/main/java/com/darwinreforged/server/core/modules/PluginModuleNative.java
+++ Core/src/main/java/com/darwinreforged/server/core/modules/PluginModuleNative.java
@@ -1,13 +1,4 @@
 package com.darwinreforged.server.core.modules;
 
-import com.darwinreforged.server.core.events.internal.ServerInitEvent;
-import com.darwinreforged.server.core.events.internal.ServerReloadEvent;
-import com.darwinreforged.server.core.events.internal.ServerStartedEvent;
-
 public interface PluginModuleNative {
-    void onServerFinishLoad(ServerInitEvent event);
-
-    void onServerStart(ServerStartedEvent event);
-
-    void onServerReload(ServerReloadEvent event);
 }
