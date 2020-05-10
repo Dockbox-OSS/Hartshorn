7da6100421bad22a933330ce6e74fac8e2391e29
Guus Lieben <guus@xendox.com>
2020-05-10 19:42:29 +0200

    Add PlayerLoggedInEvent, add listeners for other events

--- Sponge/src/main/java/com/darwinreforged/server/sponge/SpongeListener.java
+++ Sponge/src/main/java/com/darwinreforged/server/sponge/SpongeListener.java
@@ -2,6 +2,8 @@ package com.darwinreforged.server.sponge;
 
 import com.darwinreforged.server.core.entities.living.DarwinPlayer;
 import com.darwinreforged.server.core.events.CancellableEvent;
+import com.darwinreforged.server.core.events.internal.InventoryInteractionEvent;
+import com.darwinreforged.server.core.events.internal.PlayerLoggedInEvent;
 import com.darwinreforged.server.core.events.internal.PlayerMoveEvent;
 import com.darwinreforged.server.core.events.internal.PlayerTeleportEvent;
 import com.darwinreforged.server.core.events.internal.ServerReloadEvent;
@@ -13,6 +15,8 @@ import org.spongepowered.api.event.Listener;
 import org.spongepowered.api.event.entity.MoveEntityEvent;
 import org.spongepowered.api.event.filter.cause.First;
 import org.spongepowered.api.event.game.GameReloadEvent;
+import org.spongepowered.api.event.item.inventory.InteractInventoryEvent;
+import org.spongepowered.api.event.network.ClientConnectionEvent;
 
 public class SpongeListener {
 
@@ -31,9 +35,18 @@ public class SpongeListener {
         postCancellable(new PlayerMoveEvent(new DarwinPlayer(p.getUniqueId(), p.getName())), event);
     }
 
+    @Listener
+    public void onPlayerLoggedIn(ClientConnectionEvent.Join event, @First Player p) {
+        DarwinServer.getEventBus().post(new PlayerLoggedInEvent(new DarwinPlayer(p.getUniqueId(), p.getName())));
+    }
+
+    @Listener
+    public void onInventoryInteract(InteractInventoryEvent event, @First Player p) {
+        postCancellable(new InventoryInteractionEvent(new DarwinPlayer(p.getUniqueId(), p.getName())), event);
+    }
+
     private <I extends CancellableEvent> void postCancellable(I e, Cancellable se) {
         DarwinServer.getEventBus().post(e);
         if (e.isCancelled()) se.setCancelled(true);
     }
-
 }
