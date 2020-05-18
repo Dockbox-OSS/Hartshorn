6c0370917413954d2025e821c4ac43ec3873f602
Guus Lieben <guus@xendox.com>
2020-05-18 23:00:29 +0200

    Fix absent color codes in chat

--- Sponge/src/main/java/com/darwinreforged/server/sponge/implementations/SpongePlayerManager.java
+++ Sponge/src/main/java/com/darwinreforged/server/sponge/implementations/SpongePlayerManager.java
@@ -42,23 +42,29 @@ import me.lucko.luckperms.api.Node;
 
 public class SpongePlayerManager extends PlayerManager {
 
+    private static Text parseColors(String s) {
+        return TextSerializers.FORMATTING_CODE.deserializeUnchecked(s);
+    }
+
     @Override
     public void broadcast(com.darwinreforged.server.core.chat.Text message) {
-        Sponge.getServer().getBroadcastChannel().send(Text.of(message));
+        Sponge.getServer().getBroadcastChannel().send(parseColors(message.getText()));
     }
 
     @Override
     public void broadcastIfPermitted(com.darwinreforged.server.core.chat.Text message, String permission) {
-        Sponge.getServer().getOnlinePlayers().parallelStream().filter(p -> p.hasPermission(permission)).forEach(p -> p.sendMessage(Text.of(message.toLegacy())));
+        Sponge.getServer().getOnlinePlayers().parallelStream().filter(p -> p.hasPermission(permission)).forEach(p -> p.sendMessage(parseColors(message.getText())));
     }
 
     @Override
     public void tell(MessageReceiver receiver, com.darwinreforged.server.core.chat.Text message) {
         if (receiver instanceof DarwinPlayer) {
+            System.out.println("Legacy === " + message.toLegacy().replaceAll("&", "SECTION"));
+            System.out.println("Text === " + message.getText().replace("\u00A7", "SECTION"));
             Sponge.getServer().getPlayer(((DarwinPlayer) receiver).getUniqueId())
-                    .ifPresent(spp -> spp.sendMessage(Text.of(Translations.DEFAULT_SINGLE_MESSAGE.f(message.toLegacy()))));
+                    .ifPresent(spp -> spp.sendMessage(parseColors(Translations.DEFAULT_SINGLE_MESSAGE.f(message.getText()))));
         } else if (receiver instanceof Console) {
-            Sponge.getServer().getConsole().sendMessage(Text.of(Translations.DEFAULT_SINGLE_MESSAGE.f(message.toLegacy())));
+            Sponge.getServer().getConsole().sendMessage(parseColors(Translations.DEFAULT_SINGLE_MESSAGE.f(message.getText())));
         } else {
             DarwinServer.getLog().warn("Failed to get receiver for : " + receiver);
         }
@@ -67,9 +73,9 @@ public class SpongePlayerManager extends PlayerManager {
     @Override
     public void tellNoMarkup(MessageReceiver receiver, com.darwinreforged.server.core.chat.Text message) {
         if (receiver instanceof DarwinPlayer) {
-            Sponge.getServer().getPlayer(((Target) receiver).getUniqueId()).ifPresent(spp -> spp.sendMessage(Text.of(message.toLegacy())));
+            Sponge.getServer().getPlayer(((Target) receiver).getUniqueId()).ifPresent(spp -> spp.sendMessage(parseColors(message.toLegacy())));
         } else if (receiver instanceof Console) {
-            Sponge.getServer().getConsole().sendMessage(Text.of(message.toLegacy()));
+            Sponge.getServer().getConsole().sendMessage(parseColors(message.toLegacy()));
         }
     }
 
