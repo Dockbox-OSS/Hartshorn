31bb70b72f0e6c65c096f4c767fca337471499a2
Guus Lieben <guus@xendox.com>
2020-05-21 22:45:20 +0200

    Fix unavailable class being used in Core (IntelliJ added it to my classpath)

--- Core/src/main/java/com/darwinreforged/server/core/resources/Dependencies.java
+++ Core/src/main/java/com/darwinreforged/server/core/resources/Dependencies.java
@@ -1,12 +1,11 @@
 package com.darwinreforged.server.core.resources;
 
 import com.boydti.fawe.Fawe;
-import com.sk89q.worldedit.WorldEdit;
 
 public enum Dependencies implements Dependency {
     // TODO? Convert to getclass of types?
     FASTASYNCWORLDEDIT(Fawe.class.toGenericString()),
-    NATIVE_WORLDEDIT(WorldEdit.class.toGenericString()),
+    NATIVE_WORLDEDIT("com.sk89q.worldedit.WorldEdit"),
     PLOTSQUARED("com.intellectualcrafters.plot.PS"),
     MULTICHAT("xyz.olivermartin.multichat.local.common.MultiChatLocal"),
     VOXELSNIPER("com.thevoxelbox.voxelsniper.VoxelSniper");
