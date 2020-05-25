eae4a8ef69c4b27adabb4ddc5273157d5534e4aa
Guus Lieben <guus@xendox.com>
2020-05-25 16:42:17 +0200

    Add harsh/soft checks for dependencies

--- Core/src/main/java/com/darwinreforged/server/core/resources/Dependencies.java
+++ Core/src/main/java/com/darwinreforged/server/core/resources/Dependencies.java
@@ -5,16 +5,19 @@ import com.sk89q.worldedit.WorldEdit;
 
 public enum Dependencies implements Dependency {
     // TODO? Convert to getclass of types?
-    FASTASYNCWORLDEDIT(Fawe.class.toGenericString()),
-    NATIVE_WORLDEDIT(WorldEdit.class.toGenericString()),
-    PLOTSQUARED("com.intellectualcrafters.plot.PS"),
-    MULTICHAT("xyz.olivermartin.multichat.local.common.MultiChatLocal"),
-    VOXELSNIPER("com.thevoxelbox.voxelsniper.VoxelSniper");
+    FASTASYNCWORLDEDIT(Fawe.class.toGenericString(), true),
+    NATIVE_WORLDEDIT(WorldEdit.class.toGenericString(), true),
+    PLOTSQUARED("com.intellectualcrafters.plot.PS", true),
+    MULTICHAT("xyz.olivermartin.multichat.local.common.MultiChatLocal", true),
+    VOXELSNIPER("com.thevoxelbox.voxelsniper.VoxelSniper", true),
+    PLACEHOLDER_API("me.clip.placeholderapi.PlaceholderAPI", false);
 
     private final String mainClass;
+    private final boolean harsh;
 
-    Dependencies(String mainClass) {
+    Dependencies(String mainClass, boolean harsh) {
         this.mainClass = mainClass;
+        this.harsh = harsh;
     }
 
     @Override
@@ -23,6 +26,8 @@ public enum Dependencies implements Dependency {
     }
 
     public boolean isLoaded() {
+        if (!harsh) return true;
+
         try {
             Class.forName(this.mainClass);
             return true;
