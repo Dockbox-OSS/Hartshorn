abd91ea2e2fe168ba5a5cc04c997824d3c8d2d6f
Guus Lieben <guus@xendox.com>
2020-06-01 15:39:12 +0200

    Refactor modules

---
+++ Core/src/main/java/com/darwinreforged/server/modules/modularwiki/WikiObject.java
@@ -0,0 +1,41 @@
+package com.darwinreforged.server.modules.modularwiki;
+
+public class WikiObject {
+
+    private String id;
+    private String name;
+    private String permission;
+    private String[] description;
+    private Boolean hidden;
+
+    public WikiObject() {
+    }
+
+    public WikiObject(String id, String name, String permission, String[] description, Boolean hidden) {
+        this.id = id;
+        this.name = name;
+        this.permission = permission;
+        this.description = description;
+        this.hidden = hidden;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPermission() {
+        return permission;
+    }
+
+    public String[] getDescription() {
+        return description;
+    }
+
+    public Boolean isHidden() {
+        return hidden != null ? hidden : Boolean.FALSE;
+    }
+}
