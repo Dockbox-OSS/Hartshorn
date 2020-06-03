b9d880d8d849ef9e8f61b348e01de31bf7b59f1c
Guus Lieben <guus@xendox.com>
2020-06-03 22:31:20 +0200

    Add dependency generation, additionally permits custom dependencies

--- Core/src/main/java/com/darwinreforged/server/core/resources/Dependencies.java
+++ Core/src/main/java/com/darwinreforged/server/core/resources/Dependencies.java
@@ -4,19 +4,18 @@ import com.boydti.fawe.Fawe;
 import com.sk89q.worldedit.WorldEdit;
 
 public enum Dependencies implements Dependency {
-    // TODO? Convert to getclass of types?
-    FASTASYNCWORLDEDIT(Fawe.class.toGenericString(), true),
-    NATIVE_WORLDEDIT(WorldEdit.class.toGenericString(), true),
-    PLOTSQUARED("com.intellectualcrafters.plot.PS", true),
-    MULTICHAT("xyz.olivermartin.multichat.local.common.MultiChatLocal", true),
-    VOXELSNIPER("com.thevoxelbox.voxelsniper.VoxelSniper", true),
-    PLACEHOLDER_API("me.clip.placeholderapi.PlaceholderAPI", false);
+    FASTASYNCWORLDEDIT(Dependency.of(Fawe.class.toGenericString()), true),
+    NATIVE_WORLDEDIT(Dependency.of(WorldEdit.class.toGenericString()), true),
+    PLOTSQUARED(Dependency.of("com.intellectualcrafters.plot.PS"), true),
+    MULTICHAT(Dependency.of("xyz.olivermartin.multichat.local.common.MultiChatLocal"), true),
+    VOXELSNIPER(Dependency.of("com.thevoxelbox.voxelsniper.VoxelSniper"), true),
+    PLACEHOLDER_API(Dependency.of("me.clip.placeholderapi.PlaceholderAPI"), false);
 
     private final String mainClass;
     private final boolean harsh;
 
-    Dependencies(String mainClass, boolean harsh) {
-        this.mainClass = mainClass;
+    Dependencies(Dependency dependency, boolean harsh) {
+        this.mainClass = dependency.getMainClass();
         this.harsh = harsh;
     }
 
