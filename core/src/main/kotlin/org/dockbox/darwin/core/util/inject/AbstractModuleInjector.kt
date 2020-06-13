fcc7cad6f680ee0d2038113b86ce929fbd98345b
Guus Lieben <guus@xendox.com>
2020-06-13 23:24:48 +0200

    Remove redundant injector

--- core/src/main/kotlin/org/dockbox/darwin/core/util/inject/AbstractModuleInjector.kt
+++ core/src/main/kotlin/org/dockbox/darwin/core/util/inject/AbstractModuleInjector.kt
@@ -1,8 +1,6 @@
 package org.dockbox.darwin.core.util.inject
 
 import com.google.inject.AbstractModule
-import org.dockbox.darwin.core.util.module.ModuleLoader
-import org.dockbox.darwin.core.util.module.ModuleScanner
 
 abstract class AbstractModuleInjector : AbstractModule() {
 }
