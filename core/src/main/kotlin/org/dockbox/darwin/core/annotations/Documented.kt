b98f433076054675064bc287e34bc45609b60a32
Guus Lieben <guus@xendox.com>
2020-06-16 13:00:37 +0200

    Remove Documented annotation

--- core/src/main/kotlin/org/dockbox/darwin/core/annotations/Documented.kt
+++
@@ -1,9 +0,0 @@
-package org.dockbox.darwin.core.annotations
-
-import org.dockbox.darwin.core.i18n.I18N
-
-@Retention(AnnotationRetention.RUNTIME)
-@Target(AnnotationTarget.FIELD)
-annotation class Documented(
-        val value: I18N
-)
