d4fda1703f7aedbd02c413d5c1eb6b0ae27af470
Guus Lieben <guus@xendox.com>
2020-06-16 13:33:03 +0200

    Prepare SpongeServer

--- sponge/src/main/java/org/dockbox/darwin/sponge/SpongeServer.java
+++ sponge/src/main/java/org/dockbox/darwin/sponge/SpongeServer.java
@@ -1,12 +1,40 @@
 package org.dockbox.darwin.sponge;
 
 import org.dockbox.darwin.core.server.CoreServer;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
-import org.slf4j.Logger;
+import org.dockbox.darwin.sponge.util.inject.SpongeExceptionInjector;
+import org.dockbox.darwin.sponge.util.inject.SpongeModuleInjector;
+import org.dockbox.darwin.sponge.util.inject.SpongeUtilInjector;
+import org.spongepowered.api.event.Listener;
+import org.spongepowered.api.event.game.state.GameInitializationEvent;
+import org.spongepowered.api.plugin.Dependency;
+import org.spongepowered.api.plugin.Plugin;
 
-import java.util.Date;
 
+@Plugin(
+        id = "darwinserver",
+        name = "Darwin Server",
+        description = "Custom plugins and modifications combined into a single source",
+        url = "https://darwinreforged.com",
+        authors = "GuusLieben",
+        dependencies = {
+                @Dependency(id = "plotsquared"),
+                @Dependency(id = "nucleus"),
+                @Dependency(id = "luckperms")
+        }
+)
 public class SpongeServer extends CoreServer {
 
+    public SpongeServer() {
+        super(new SpongeModuleInjector(), new SpongeExceptionInjector(), new SpongeUtilInjector());
+    }
+
+    @Listener
+    public void onServerInit(GameInitializationEvent event) {
+        // TODO : Init event listener
+        Iterable<Class<?>> annotatedCandidates = CoreServer.getModuleScanner()
+                .collectClassCandidates("org.dockbox.darwin.integrated")
+                .getAnnotatedCandidates();
+        annotatedCandidates.forEach(module -> CoreServer.getModuleLoader().loadModule(module));
+        // TODO : Post init event to modules
+    }
 }
