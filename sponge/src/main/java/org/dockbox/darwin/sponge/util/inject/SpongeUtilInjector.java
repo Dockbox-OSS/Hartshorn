115c0a7755037c334200df1ba4b0992d4cee2aa4
Guus Lieben <guus@xendox.com>
2020-06-16 20:11:25 +0200

    Implement EventBus

--- sponge/src/main/java/org/dockbox/darwin/sponge/util/inject/SpongeUtilInjector.java
+++ sponge/src/main/java/org/dockbox/darwin/sponge/util/inject/SpongeUtilInjector.java
@@ -1,5 +1,7 @@
 package org.dockbox.darwin.sponge.util.inject;
 
+import org.dockbox.darwin.core.util.events.EventBus;
+import org.dockbox.darwin.core.util.events.SimpleEventBus;
 import org.dockbox.darwin.core.util.files.ConfigManager;
 import org.dockbox.darwin.core.util.files.DataManager;
 import org.dockbox.darwin.core.util.files.FileUtils;
@@ -15,6 +17,7 @@ public class SpongeUtilInjector extends AbstractUtilInjector {
         bind(FileUtils.class).to(SpongeFileUtils.class);
         bind(ConfigManager.class).to(YamlConfigManager.class);
         bind(DataManager.class).to(YamlSQLiteDataManager.class);
+        bind(EventBus.class).to(SimpleEventBus.class);
     }
 
 }
