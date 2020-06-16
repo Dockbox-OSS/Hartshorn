67139ed65433d67599be7038c812663ffdb88285
Guus Lieben <guus@xendox.com>
2020-06-16 17:44:13 +0200

    Indicate NMS access and min/preferred versions for server types

--- core/src/main/kotlin/org/dockbox/darwin/core/server/Server.kt
+++ core/src/main/kotlin/org/dockbox/darwin/core/server/Server.kt
@@ -12,8 +12,12 @@ interface Server {
     fun except(msg: String?, vararg e: Throwable?)
     fun getServerType(): ServerType
 
-    enum class ServerType {
-        SPONGE, MAGMA, SPIGOT, PAPER, OTHER
+    enum class ServerType(val hasNMSAccess: Boolean, val minimumVersion: String, val preferredVersion: String) {
+        SPONGE(true, "1.12.2-2555-7.1.0-BETA-2815", "1.12.2-2838-7.2.2-RC0"),
+        MAGMA(true, "Not (yet) supported", "Not (yet) supported"),
+        SPIGOT(true, "Not (yet) supported", "Not (yet) supported"),
+        PAPER(true, "Not (yet) supported", "Not (yet) supported"),
+        OTHER(true, "Not (yet) supported", "Not (yet) supported")
     }
 
 }
