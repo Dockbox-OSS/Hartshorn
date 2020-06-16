688a1eb2e30c186ac8994ae80647f0771a0f0d23
Guus Lieben <guus@xendox.com>
2020-06-16 17:21:26 +0200

    Implement Yaml/SQLite DataManger

--- core/src/main/java/org/dockbox/darwin/core/util/files/YamlSQLiteDataManager.java
+++ core/src/main/java/org/dockbox/darwin/core/util/files/YamlSQLiteDataManager.java
@@ -1,107 +1,211 @@
 package org.dockbox.darwin.core.util.files;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+import com.fasterxml.jackson.dataformat.yaml.YAMLGenerator.Feature;
 import com.j256.ormlite.dao.Dao;
+import com.j256.ormlite.dao.DaoManager;
+import com.j256.ormlite.jdbc.JdbcConnectionSource;
+import com.j256.ormlite.support.ConnectionSource;
+import com.j256.ormlite.table.TableUtils;
 
+import org.dockbox.darwin.core.server.CoreServer;
+import org.dockbox.darwin.core.server.ServerReference;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.File;
+import java.io.IOException;
 import java.nio.file.Path;
+import java.sql.SQLException;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
-public class YamlSQLiteDataManager implements DataManager<Dao<?, ?>> {
+public class YamlSQLiteDataManager extends ServerReference implements DataManager<Dao<?, ?>> {
+
+    private final ObjectMapper mapper;
+    private static final Map<String, ConnectionSource> jdbcSources = new ConcurrentHashMap<>();
+    private static final String JDBC_FORMAT = "jdbc:sqlite:%s";
+
+    public YamlSQLiteDataManager() {
+        YAMLFactory fact = new YAMLFactory();
+        fact.disable(Feature.WRITE_DOC_START_MARKER);
+        mapper = new ObjectMapper(fact);
+
+        mapper.findAndRegisterModules();
+        mapper.setVisibility(PropertyAccessor.ALL, Visibility.ANY);
+        mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        mapper.disable(SerializationFeature.WRAP_ROOT_VALUE);
+        mapper.enable(SerializationFeature.ORDER_MAP_ENTRIES_BY_KEYS);
+    }
 
     @NotNull
     @Override
     public Path getDataDir(@NotNull Class<?> module) {
-        return null;
+        return getModuleAndCallback(module, (annotation) -> getInstance(FileUtils.class).getDataDir().resolve(annotation.id()));
     }
 
     @NotNull
     @Override
     public Path getDataDir(@NotNull Object module) {
-        return null;
+        if (!(module instanceof Class)) module = module.getClass();
+        return getDataDir((Class<?>) module);
     }
 
     @NotNull
     @Override
     public File getDefaultDataFile(@NotNull Class<?> module) {
-        return null;
+        return getModuleAndCallback(module, (annotation) -> {
+            Path dataPath = getDataDir(module);
+            return getInstance(FileUtils.class).createFileIfNotExists(new File(dataPath.toFile(), annotation.id() + ".yml"));
+        });
     }
 
     @NotNull
     @Override
     public File getDefaultDataFile(@NotNull Object module) {
-        return null;
+        if (!(module instanceof Class)) module = module.getClass();
+        return getDefaultDataFile((Class<?>) module);
     }
 
     @NotNull
     @Override
     public File getDefaultBulkDataFile(@NotNull Class<?> module) {
-        return null;
+        return getModuleAndCallback(module, (annotation) -> {
+            Path dataPath = getDataDir(module);
+            return getInstance(FileUtils.class).createFileIfNotExists(new File(dataPath.toFile(), annotation.id() + ".db"));
+        });
     }
 
     @NotNull
     @Override
     public File getDefaultBulkDataFile(@NotNull Object module) {
-        return null;
+        if (!(module instanceof Class)) module = module.getClass();
+        return getDefaultBulkDataFile((Class<?>) module);
     }
 
     @Override
-    public Dao<?, ?> getBulkDao(@NotNull Class<?> module, @NotNull Class<?> type, @NotNull File file) {
-        return null;
+    public Dao<?, ?> getBulkDao(@NotNull Class<?> module, @NotNull Class<?> type, @NotNull String fileName) {
+        return getModuleAndCallback(module, (annotation) -> {
+            Path dataDir = getDataDir(module);
+            return constructDao(type, new File(dataDir.toFile(), fileName));
+        });
     }
 
     @Override
-    public Dao<?, ?> getBulkDao(@NotNull Object module, @NotNull Class<?> type, @NotNull File file) {
-        return null;
+    public Dao<?, ?> getBulkDao(@NotNull Object module, @NotNull Class<?> type, @NotNull String fileName) {
+        if (!(module instanceof Class)) module = module.getClass();
+        return getBulkDao((Class<?>) module, type, fileName);
     }
 
     @Override
-    public Dao<?, ?> getBulkDao(@NotNull Class<?> type, @NotNull File file) {
-        return null;
+    public Dao<?, ?> getDefaultBulkDao(@NotNull Class<?> module, @NotNull Class<?> type) {
+        return constructDao(type, getDefaultBulkDataFile(module));
     }
 
+    @Override
+    public Dao<?, ?> getDefaultBulkDao(@NotNull Object module, @NotNull Class<?> type) {
+        if (!(module instanceof Class)) module = module.getClass();
+        return constructDao(type, getDefaultBulkDataFile(module));
+    }
+
+    @SuppressWarnings({"rawtypes", "unchecked"})
     @NotNull
     @Override
     public Map<String, Object> getDataContents(@NotNull Class<?> module) {
-        return null;
+        return getDataContents(module, Map.class, new HashMap());
     }
 
+    @SuppressWarnings({"rawtypes", "unchecked"})
     @NotNull
     @Override
     public Map<String, Object> getDataContents(@NotNull Object module) {
-        return null;
-    }
-
-    @Override
-    public void writeToData(@NotNull Class<?> module, @NotNull Map<String, ?> data) {
-
-    }
-
-    @Override
-    public void writeToData(@NotNull Object module, @NotNull Map<String, ?> data) {
-
+        return getDataContents(module, Map.class, new HashMap());
     }
 
     @NotNull
     @Override
     public <T> T getDataContents(@NotNull Class<?> module, @NotNull Class<T> convertTo, T defaultValue) {
-        return null;
+        return getModuleAndCallback(module, (annotation) -> {
+            try {
+                File cf = getDefaultDataFile(module);
+                T res = mapper.readValue(cf, convertTo);
+                return res != null ? res : defaultValue;
+            } catch (IOException | IllegalArgumentException e) {
+                CoreServer.getServer().except("Failed to map data contents", e);
+            }
+            return defaultValue;
+        });
     }
 
     @NotNull
     @Override
     public <T> T getDataContents(@NotNull Object module, @NotNull Class<T> convertTo, T defaultValue) {
-        return null;
+        if (!(module instanceof Class)) module = module.getClass();
+        return getDataContents((Class<?>) module, convertTo, defaultValue);
     }
 
     @Override
     public <T> void writeToData(@NotNull Class<?> module, T data) {
-
+        try {
+            File df = getDefaultDataFile(module);
+            mapper.writeValue(df, data);
+        } catch (IOException e) {
+            CoreServer.getServer().except("Failed to write data contents", e);
+        }
     }
 
     @Override
     public <T> void writeToData(@NotNull Object module, T data) {
+        if (!(module instanceof Class)) module = module.getClass();
+        writeToData((Class<?>) module, data);
+    }
 
+    @Override
+    public <T> void writeToData(@NotNull Class<?> module, T data, @NotNull String fileName) {
+        try {
+            Path dataDir = getDataDir(module);
+            File df = getInstance(FileUtils.class).createFileIfNotExists(new File(dataDir.toFile(), fileName));
+            mapper.writeValue(df, data);
+        } catch (IOException e) {
+            CoreServer.getServer().except("Failed to write data contents", e);
+        }
+    }
+
+    @Override
+    public <T> void writeToData(@NotNull Object module, T data, @NotNull String fileName) {
+        if (!(module instanceof Class)) module = module.getClass();
+        writeToData((Class<?>) module, data, fileName);
+    }
+
+    public Dao<?, ?> constructDao(@NotNull Class<?> type, @NotNull File file) {
+        getInstance(FileUtils.class).createFileIfNotExists(file);
+        if (sqliteConnectedTo(file)) {
+            ConnectionSource source = jdbcSources.get(file.toString());
+            try {
+                TableUtils.createTableIfNotExists(source, type);
+                return DaoManager.createDao(source, type);
+            } catch (SQLException e) {
+                CoreServer.getServer().except("Could not create DAO for type", e);
+            }
+        }
+        return null;
+    }
+
+    public boolean sqliteConnectedTo(File file) {
+        String fileAb = file.toString();
+        if (jdbcSources.containsKey(fileAb) && jdbcSources.get(fileAb) != null) return true;
+        String connStr = String.format(JDBC_FORMAT, fileAb);
+        try {
+            ConnectionSource connectionSource = new JdbcConnectionSource(connStr);
+            jdbcSources.put(fileAb, connectionSource);
+            return true;
+        } catch (SQLException e) {
+            CoreServer.getServer().except(String.format("Failed to create connection source for '%s'", fileAb), e);
+            return false;
+        }
     }
 }
--- core/src/main/kotlin/org/dockbox/darwin/core/util/files/DataManager.kt
+++ core/src/main/kotlin/org/dockbox/darwin/core/util/files/DataManager.kt
@@ -14,20 +14,20 @@ interface DataManager<D> {
     fun getDefaultBulkDataFile(module: Class<*>): File
     fun getDefaultBulkDataFile(module: Any): File
 
-    fun getBulkDao(module: Class<*>, type: Class<*>, file: File): D
-    fun getBulkDao(module: Any, type: Class<*>, file: File): D
-    fun getBulkDao(type: Class<*>, file: File): D
-
     fun getDataContents(module: Class<*>): Map<String, Any>
     fun getDataContents(module: Any): Map<String, Any>
 
     fun <T> getDataContents(module: Class<*>, convertTo: Class<T>, defaultValue: T): T
     fun <T> getDataContents(module: Any, convertTo: Class<T>, defaultValue: T): T
 
-    fun writeToData(module: Class<*>, data: Map<String, Any>)
-    fun writeToData(module: Any, data: Map<String, Any>)
-
     fun <T> writeToData(module: Class<*>, data: T)
     fun <T> writeToData(module: Any, data: T)
+    fun <T> writeToData(module: Class<*>, data: T, fileName: String)
+    fun <T> writeToData(module: Any, data: T, fileName: String)
+
+    fun getBulkDao(module: Any, type: Class<*>, fileName: String): D
+    fun getBulkDao(module: Class<*>, type: Class<*>, fileName: String): D
+    fun getDefaultBulkDao(module: Any, type: Class<*>): D
+    fun getDefaultBulkDao(module: Class<*>, type: Class<*>): D
 
 }
