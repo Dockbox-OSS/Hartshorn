812d82b7fa0464390f917dbe32f61a5b25a4a49b
Guus Lieben <GLieben@costengineering.eu>
2020-08-18 16:36:48 +0200

    Add constant empty UUID

---
+++ core/src/main/java/org/dockbox/darwin/core/util/uuid/UUIDUtil.java
@@ -0,0 +1,9 @@
+package org.dockbox.darwin.core.util.uuid;
+
+import java.util.UUID;
+
+public class UUIDUtil {
+
+    public static final UUID empty = UUID.fromString("00000000-1111-2222-3333-000000000000");
+
+}
