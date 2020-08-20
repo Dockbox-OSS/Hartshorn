d26c9be009fd36d94aec4380aa9b4b58dfba9052
Guus Lieben <guus@xendox.com>
2020-08-20 12:00:53 +0200

    Additional getWorld for Locatable

--- core/src/main/kotlin/org/dockbox/darwin/core/objects/targets/Locatable.kt
+++ core/src/main/kotlin/org/dockbox/darwin/core/objects/targets/Locatable.kt
@@ -1,10 +1,12 @@
 package org.dockbox.darwin.core.objects.targets
 
 import org.dockbox.darwin.core.objects.location.Location
+import org.dockbox.darwin.core.objects.location.World
 
 interface Locatable : Target {
 
     fun getLocation(): Location
     fun setLocation(location: Location)
+    fun getWorld(): World
 
 }
