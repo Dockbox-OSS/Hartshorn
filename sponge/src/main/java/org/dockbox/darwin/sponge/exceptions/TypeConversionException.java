f88343e0f6206e6241338ee95a0ee80e84b550b2
Guus Lieben <guus@xendox.com>
2020-08-20 16:37:33 +0200

    Add text type conversion (internal to Sponge)

---
+++ sponge/src/main/java/org/dockbox/darwin/sponge/exceptions/TypeConversionException.java
@@ -0,0 +1,11 @@
+package org.dockbox.darwin.sponge.exceptions;
+
+public class TypeConversionException extends RuntimeException {
+
+    public TypeConversionException() {
+    }
+
+    public TypeConversionException(String message) {
+        super(message);
+    }
+}
--- sponge/src/main/java/org/dockbox/darwin/sponge/util/SpongeConversionUtil.java
+++ sponge/src/main/java/org/dockbox/darwin/sponge/util/SpongeConversionUtil.java
@@ -2,31 +2,100 @@ package org.dockbox.darwin.sponge.util;
 
 import com.flowpowered.math.vector.Vector3d;
 
+import org.dockbox.darwin.core.objects.targets.CommandSource;
 import org.dockbox.darwin.core.objects.tuple.Vector3D;
 import org.dockbox.darwin.core.objects.user.Gamemode;
+import org.dockbox.darwin.core.text.actions.ClickAction;
+import org.dockbox.darwin.core.text.actions.HoverAction;
+import org.dockbox.darwin.core.text.actions.ShiftClickAction;
+import org.dockbox.darwin.sponge.exceptions.TypeConversionException;
 import org.dockbox.darwin.sponge.objects.location.SpongeLocation;
+import org.dockbox.darwin.sponge.objects.location.SpongePlayer;
 import org.dockbox.darwin.sponge.objects.location.SpongeWorld;
+import org.dockbox.darwin.sponge.objects.targets.SpongeConsole;
 import org.jetbrains.annotations.NotNull;
 import org.spongepowered.api.Sponge;
+import org.spongepowered.api.command.source.ConsoleSource;
 import org.spongepowered.api.entity.living.player.gamemode.GameMode;
 import org.spongepowered.api.entity.living.player.gamemode.GameModes;
 import org.spongepowered.api.text.Text;
+import org.spongepowered.api.text.action.TextActions;
+import org.spongepowered.api.text.serializer.TextSerializers;
 import org.spongepowered.api.world.Location;
 import org.spongepowered.api.world.World;
+
+import java.net.URL;
+import java.util.function.Consumer;
 
 public class SpongeConversionUtil {
 
     @NotNull
     public static Text toSponge(org.dockbox.darwin.core.text.Text message) {
-        // TODO
-        message.append("");
-        return Text.EMPTY;
+        Iterable<org.dockbox.darwin.core.text.Text> parts = message.getParts();
+        Text.Builder b = Text.builder();
+        parts.forEach(part -> {
+            Text.Builder pb = Text.builder();
+            pb.append(TextSerializers.FORMATTING_CODE.deserialize(part.toLegacy()));
+
+            org.spongepowered.api.text.action.ClickAction<?> clickAction = toSponge(part.getClickAction());
+            if (clickAction != null) pb.onClick(clickAction);
+
+            org.spongepowered.api.text.action.HoverAction<?> hoverAction = toSponge(part.getHoverAction());
+            if (hoverAction != null) pb.onHover(hoverAction);
+
+            org.spongepowered.api.text.action.ShiftClickAction<?> shiftClickAction = toSponge(part.getShiftClickAction());
+            if (shiftClickAction != null) pb.onShiftClick(shiftClickAction);
+
+            b.append(pb.build());
+        });
+
+        return b.build();
     }
 
-    @NotNull
-    public static org.dockbox.darwin.core.text.Text fromSponge(Text message) {
-        // TODO
-        return new org.dockbox.darwin.core.text.Text();
+    private static org.spongepowered.api.text.action.ShiftClickAction<?> toSponge(ShiftClickAction<?> action) {
+        if (action == null) return null;
+        Object result = action.getResult();
+        if (action instanceof ShiftClickAction.InsertText) {
+            return TextActions.insertText(((org.dockbox.darwin.core.text.Text) result).toPlain());
+        }
+        return null;
+    }
+
+    private static org.spongepowered.api.text.action.HoverAction<?> toSponge(HoverAction<?> action) {
+        if (action == null) return null;
+        Object result = action.getResult();
+        if (action instanceof HoverAction.ShowText) {
+            return TextActions.showText(TextSerializers.FORMATTING_CODE.deserialize(((org.dockbox.darwin.core.text.Text) result).toLegacy()));
+        }
+        // TODO: Once implemented; ShowItem, ShowEntity
+        return null;
+    }
+
+    private static org.spongepowered.api.text.action.ClickAction<?> toSponge(ClickAction<?> action) {
+        if (action == null) return null;
+        Object result = action.getResult();
+        if (action instanceof ClickAction.OpenUrl) {
+            return TextActions.openUrl((URL) result);
+        } else if (action instanceof ClickAction.RunCommand) {
+            return TextActions.runCommand((String) result);
+        } else if (action instanceof ClickAction.ChangePage) {
+            return TextActions.changePage((int) result);
+        } else if (action instanceof ClickAction.SuggestCommand) {
+            return TextActions.suggestCommand((String) result);
+        } else if (action instanceof ClickAction.ExecuteCallback) {
+            return TextActions.executeCallback(commandSource -> {
+                Consumer<CommandSource> consumer = ((ClickAction.ExecuteCallback) action).getResult();
+                consumer.accept(fromSponge(commandSource));
+            });
+        }
+        return null;
+    }
+
+    private static CommandSource fromSponge(org.spongepowered.api.command.CommandSource commandSource) {
+        if (commandSource instanceof ConsoleSource) return SpongeConsole.instance;
+        else if (commandSource instanceof org.spongepowered.api.entity.living.player.Player)
+            return new SpongePlayer(((org.spongepowered.api.entity.living.player.Player) commandSource).getUniqueId(), commandSource.getName());
+        throw new TypeConversionException("Could not convert CommandSource type '" + commandSource.getClass().getCanonicalName() + "'");
     }
 
     @NotNull
