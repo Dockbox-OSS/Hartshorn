aaba57ccca328ec3958e36779fcc35a2d18da357
Guus Lieben <guus@xendox.com>
2020-08-20 14:49:40 +0200

    Move TextConversionUtil to common Sponge conversion

---
+++ sponge/src/main/java/org/dockbox/darwin/sponge/util/SpongeConversionUtil.java
@@ -0,0 +1,69 @@
+package org.dockbox.darwin.sponge.util;
+
+import org.dockbox.darwin.core.objects.user.Gamemode;
+import org.jetbrains.annotations.NotNull;
+import org.spongepowered.api.entity.living.player.gamemode.GameMode;
+import org.spongepowered.api.entity.living.player.gamemode.GameModes;
+import org.spongepowered.api.text.Text;
+import org.spongepowered.api.world.Location;
+import org.spongepowered.api.world.World;
+
+public class SpongeConversionUtil {
+
+    @NotNull
+    public static Text toSponge(org.dockbox.darwin.core.text.Text message) {
+        // TODO
+        message.append("");
+        return Text.EMPTY;
+    }
+
+    @NotNull
+    public static org.dockbox.darwin.core.text.Text fromSponge(Text message) {
+        // TODO
+        return new org.dockbox.darwin.core.text.Text();
+    }
+
+    @NotNull
+    public static Location<World> toSponge(org.dockbox.darwin.core.objects.location.Location location) {
+        return null;
+    }
+
+    @NotNull
+    public static org.dockbox.darwin.core.objects.location.Location fromSponge(Location<?> location) {
+        return org.dockbox.darwin.core.objects.location.Location.Companion.getEMPTY();
+    }
+
+    @NotNull
+    public static World toSponge(org.dockbox.darwin.core.objects.location.World world) {
+        return null;
+    }
+
+    @NotNull
+    public static org.dockbox.darwin.core.objects.location.World fromSponge(World world) {
+        return org.dockbox.darwin.core.objects.location.World.Companion.getEmpty();
+    }
+
+    public static GameMode toSponge(Gamemode gamemode) {
+        switch (gamemode) {
+            case SURVIVAL:
+                return GameModes.SURVIVAL;
+            case CREATIVE:
+                return GameModes.CREATIVE;
+            case ADVENTURE:
+                return GameModes.ADVENTURE;
+            case SPECTATOR:
+                return GameModes.SPECTATOR;
+            case OTHER:
+            default:
+                return GameModes.NOT_SET;
+        }
+    }
+
+    public static Gamemode fromSponge(GameMode gamemode) {
+        try {
+            return Enum.valueOf(Gamemode.class, gamemode.toString());
+        } catch (IllegalArgumentException | NullPointerException e) {
+            return Gamemode.OTHER;
+        }
+    }
+}
