fbd88c42154b679eba098a865c6db49dbf6ce847
Guus Lieben <guus@xendox.com>
2020-08-25 17:44:41 +0200

    Add supported languages enum

---
+++ core/src/main/kotlin/org/dockbox/darwin/core/i18n/Languages.kt
@@ -0,0 +1,10 @@
+package org.dockbox.darwin.core.i18n
+
+enum class Languages(val code: String, val description: String) {
+
+    EN_US("en_US", "English (US)"),
+    NL_NL("nl_NL", "Dutch (Nederlands)"),
+    FR_FR("fr_FR", "French (Français)"),
+    DE_DE("en_US", "German (Deutsch)")
+
+}
