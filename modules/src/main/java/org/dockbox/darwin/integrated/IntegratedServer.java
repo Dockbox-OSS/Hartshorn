7a7eeed1a1bcf17682a5f66f7745dc62d33268d7
Guus Lieben <guus@xendox.com>
2020-08-26 16:35:16 +0200

    Add project for integrated modules

---
+++ modules/src/main/java/org/dockbox/darwin/integrated/IntegratedServer.java
@@ -0,0 +1,10 @@
+package org.dockbox.darwin.integrated;
+
+public class IntegratedServer {
+
+    public static void main(String[] args) {
+        System.out.println("DarwinServer is a framework plugin, it should not be started as a separate application.");
+        System.exit(8);
+    }
+
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,2 @@
 
-include("sponge", "core")
+include("sponge", "core", "modules")
