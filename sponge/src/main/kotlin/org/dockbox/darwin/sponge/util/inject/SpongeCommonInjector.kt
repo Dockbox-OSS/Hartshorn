85c0e9e105548cb393a375521973c898ee082c1e
Guus Lieben <guus@xendox.com>
2020-09-08 22:51:40 +0200

    Add missing binding for CommandBus and sort bindings

--- sponge/src/main/kotlin/org/dockbox/darwin/sponge/util/inject/SpongeCommonInjector.kt
+++ sponge/src/main/kotlin/org/dockbox/darwin/sponge/util/inject/SpongeCommonInjector.kt
@@ -17,6 +17,7 @@
 
 package org.dockbox.darwin.sponge.util.inject
 
+import org.dockbox.darwin.core.command.CommandBus
 import org.dockbox.darwin.core.i18n.I18nService
 import org.dockbox.darwin.core.i18n.SimpleI18NService
 import org.dockbox.darwin.core.server.config.DefaultGlobalConfig
@@ -33,6 +34,7 @@ import org.dockbox.darwin.core.util.extension.SimpleExtensionManager
 import org.dockbox.darwin.core.util.player.PlayerStorageService
 import org.dockbox.darwin.core.util.text.BroadcastService
 import org.dockbox.darwin.core.util.text.SimpleBroadcastService
+import org.dockbox.darwin.sponge.util.command.SpongeCommandBus
 import org.dockbox.darwin.sponge.util.discord.SpongeDiscordUtils
 import org.dockbox.darwin.sponge.util.files.SpongeFileUtils
 import org.dockbox.darwin.sponge.util.player.SpongePlayerStorageService
@@ -47,15 +49,17 @@ class SpongeCommonInjector : AbstractCommonInjector() {
     }
 
     override fun configureUtilInject() {
-        bind(FileUtils::class.java).to(SpongeFileUtils::class.java)
+        // Keep this alphabatically sorted if/when adding and/or swapping bindings (based on the interface type)
+        bind(BroadcastService::class.java).to(SimpleBroadcastService::class.java)
+        bind(BulkDataManager::class.java).to(SQLiteBulkDataManager::class.java)
+        bind(CommandBus::class.java).to(SpongeCommandBus::class.java)
         bind(ConfigManager::class.java).to(YamlConfigManager::class.java)
         bind(DataManager::class.java).to(YamlDataManager::class.java)
-        bind(BulkDataManager::class.java).to(SQLiteBulkDataManager::class.java)
-        bind(EventBus::class.java).to(SimpleEventBus::class.java)
         bind(DiscordUtils::class.java).to(SpongeDiscordUtils::class.java)
-        bind(BroadcastService::class.java).to(SimpleBroadcastService::class.java)
-        bind(PlayerStorageService::class.java).to(SpongePlayerStorageService::class.java)
-        bind(I18nService::class.java).to(SimpleI18NService::class.java)
+        bind(EventBus::class.java).to(SimpleEventBus::class.java)
+        bind(FileUtils::class.java).to(SpongeFileUtils::class.java)
         bind(GlobalConfig::class.java).to(DefaultGlobalConfig::class.java)
+        bind(I18nService::class.java).to(SimpleI18NService::class.java)
+        bind(PlayerStorageService::class.java).to(SpongePlayerStorageService::class.java)
     }
 }
