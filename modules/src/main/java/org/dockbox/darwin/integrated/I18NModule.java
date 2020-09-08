1e56e0c51db4beb781faa848b679badfa2df19f2
Guus Lieben <guus@xendox.com>
2020-09-08 22:52:08 +0200

    Fix incorrect command context for I18N

--- modules/src/main/java/org/dockbox/darwin/integrated/I18NModule.java
+++ modules/src/main/java/org/dockbox/darwin/integrated/I18NModule.java
@@ -18,7 +18,6 @@
 package org.dockbox.darwin.integrated;
 
 import org.dockbox.darwin.core.annotations.Command;
-import org.dockbox.darwin.core.util.extension.Extension;
 import org.dockbox.darwin.core.command.context.CommandContext;
 import org.dockbox.darwin.core.command.context.CommandValue;
 import org.dockbox.darwin.core.command.parse.AbstractTypeArgumentParser;
@@ -26,6 +25,8 @@ import org.dockbox.darwin.core.i18n.I18N;
 import org.dockbox.darwin.core.i18n.Languages;
 import org.dockbox.darwin.core.objects.targets.CommandSource;
 import org.dockbox.darwin.core.objects.user.Player;
+import org.dockbox.darwin.core.text.Text;
+import org.dockbox.darwin.core.util.extension.Extension;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Optional;
@@ -33,7 +34,7 @@ import java.util.Optional;
 @Extension(id = "i18n_commands", name = "I18N Commands", description = "Provided I18N commands, implementation of i18n extension", authors = {"GuusLieben"})
 public class I18NModule {
 
-    @Command(aliases = {"lang", "language"}, usage = "language <language>", context = "language <language{String}")
+    @Command(aliases = {"lang", "language"}, usage = "language <language>", context = "language <language{String}>")
     public void switchLang(CommandSource src, CommandContext ctx) {
         if (src instanceof Player) {
             Optional<Languages> ol = ctx.getArgumentAndParse("language", new LanguageArgumentParser());
@@ -42,6 +43,8 @@ public class I18NModule {
             ((Player) src).setLanguage(lang);
             // Messages sent after language switch will be in the preferred language
             src.sendWithPrefix(I18N.LANG_SWITCHED.format(lang.getDescription()));
+        } else {
+            src.send(Text.Companion.of("What are you??"));
         }
     }
 
