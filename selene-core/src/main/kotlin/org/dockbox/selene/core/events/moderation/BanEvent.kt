73fe761b012b311dda0c8d65650d75eacb13ad7b
Guus Lieben <guus@xendox.com>
2020-11-09 19:28:50 +0100

    Contracts for common ban events

---
+++ selene-core/src/main/kotlin/org/dockbox/selene/core/events/moderation/BanEvent.kt
@@ -0,0 +1,82 @@
+/*
+ *  Copyright (C) 2020 Guus Lieben
+ *
+ *  This framework is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as
+ *  published by the Free Software Foundation, either version 2.1 of the
+ *  License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See
+ *  the GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this library. If not, see {@literal<http://www.gnu.org/licenses/>}.
+ */
+
+package org.dockbox.selene.core.events.moderation
+
+import java.net.InetAddress
+import java.time.LocalDateTime
+import org.dockbox.selene.core.events.AbstractCancellableEvent
+import org.dockbox.selene.core.objects.optional.Exceptional
+import org.dockbox.selene.core.objects.targets.CommandSource
+import org.dockbox.selene.core.objects.targets.Target
+
+abstract class BanEvent<T>(
+        val target: T,
+        val reason: Exceptional<String>,
+        val source: CommandSource,
+        val expiration: Exceptional<LocalDateTime>,
+        val creation: Exceptional<LocalDateTime>
+) : AbstractCancellableEvent() {
+
+    class PlayerBannedEvent(
+            target: Target,
+            reason: Exceptional<String>,
+            source: CommandSource,
+            expiration: Exceptional<LocalDateTime>,
+            creation: Exceptional<LocalDateTime>
+    ) : BanEvent<Target>(target, reason, source, expiration, creation)
+
+    class IpBannedEvent(
+            host: InetAddress,
+            reason: Exceptional<String>,
+            source: CommandSource,
+            expiration: Exceptional<LocalDateTime>,
+            creation: Exceptional<LocalDateTime>
+    ) : BanEvent<InetAddress>(host, reason, source, expiration, creation)
+
+    class NameBannedEvent(
+            name: String,
+            reason: Exceptional<String>,
+            source: CommandSource,
+            expiration: Exceptional<LocalDateTime>,
+            creation: Exceptional<LocalDateTime>
+    ) : BanEvent<String>(name, reason, source, expiration, creation)
+
+    class PlayerUnbannedEvent(
+            target: Target,
+            reason: Exceptional<String>,
+            source: CommandSource,
+            expiration: Exceptional<LocalDateTime>,
+            creation: Exceptional<LocalDateTime>
+    ) : BanEvent<Target>(target, reason, source, expiration, creation)
+
+    class IpUnbannedEvent(
+            host: InetAddress,
+            reason: Exceptional<String>,
+            source: CommandSource,
+            expiration: Exceptional<LocalDateTime>,
+            creation: Exceptional<LocalDateTime>
+    ) : BanEvent<InetAddress>(host, reason, source, expiration, creation)
+
+    class NameUnbannedEvent(
+            name: String,
+            reason: Exceptional<String>,
+            source: CommandSource,
+            expiration: Exceptional<LocalDateTime>,
+            creation: Exceptional<LocalDateTime>
+    ) : BanEvent<String>(name, reason, source, expiration, creation)
+}
