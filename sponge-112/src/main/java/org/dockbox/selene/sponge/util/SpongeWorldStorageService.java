d8acdbfb74938bdb18b6e4ccf186ebbfe57bd0fe
Guus Lieben <guus@xendox.com>
2020-11-28 19:54:23 +0100

    Method references

--- sponge-112/src/main/java/org/dockbox/selene/sponge/util/SpongeWorldStorageService.java
+++ sponge-112/src/main/java/org/dockbox/selene/sponge/util/SpongeWorldStorageService.java
@@ -5,6 +5,7 @@ import org.dockbox.selene.core.objects.optional.Exceptional;
 import org.dockbox.selene.core.util.world.WorldStorageService;
 import org.jetbrains.annotations.NotNull;
 import org.spongepowered.api.Sponge;
+import org.spongepowered.api.world.storage.WorldProperties;
 
 import java.util.List;
 import java.util.UUID;
@@ -15,24 +16,24 @@ public class SpongeWorldStorageService extends WorldStorageService {
     @NotNull
     @Override
     public List<World> getLoadedWorlds() {
-        return Sponge.getServer().getWorlds().stream().map(world -> SpongeConversionUtil.fromSponge(world)).collect(Collectors.toList());
+        return Sponge.getServer().getWorlds().stream().map(SpongeConversionUtil::fromSponge).collect(Collectors.toList());
     }
 
     @NotNull
     @Override
     public List<UUID> getAllWorldUUIDs() {
-        return Sponge.getServer().getAllWorldProperties().stream().map(world -> world.getUniqueId()).collect(Collectors.toList());
+        return Sponge.getServer().getAllWorldProperties().stream().map(WorldProperties::getUniqueId).collect(Collectors.toList());
     }
 
     @NotNull
     @Override
     public Exceptional<World> getWorld(@NotNull String name) {
-        return Exceptional.of(Sponge.getServer().loadWorld(name)).map(world -> SpongeConversionUtil.fromSponge(world));
+        return Exceptional.of(Sponge.getServer().loadWorld(name)).map(SpongeConversionUtil::fromSponge);
     }
 
     @NotNull
     @Override
     public Exceptional<World> getWorld(@NotNull UUID uuid) {
-        return Exceptional.of(Sponge.getServer().loadWorld(uuid)).map(world -> SpongeConversionUtil.fromSponge(world));
+        return Exceptional.of(Sponge.getServer().loadWorld(uuid)).map(SpongeConversionUtil::fromSponge);
     }
 }
