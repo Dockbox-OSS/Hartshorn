3845b1cd42a0a255b04dc2a50352ca632203b180
Guus Lieben <guus@xendox.com>
2020-11-28 16:55:56 +0100

    Migrate sponge.exceptions

---
+++ sponge-112/src/main/java/org/dockbox/selene/sponge/exceptions/TypeConversionException.java
@@ -0,0 +1,8 @@
+package org.dockbox.selene.sponge.exceptions;
+
+public class TypeConversionException extends RuntimeException {
+
+    public TypeConversionException(String message) {
+        super(message);
+    }
+}
