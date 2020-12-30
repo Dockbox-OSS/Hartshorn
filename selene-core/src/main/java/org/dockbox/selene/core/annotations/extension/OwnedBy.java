81c550150a6678638755083dfaf1f98573012008
Guus Lieben <guus@xendox.com>
2020-12-30 15:30:16 +0100

    Remove default type from `@OwnedBy`

--- selene-core/src/main/java/org/dockbox/selene/core/annotations/extension/OwnedBy.java
+++ selene-core/src/main/java/org/dockbox/selene/core/annotations/extension/OwnedBy.java
@@ -25,5 +25,5 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
 public @interface OwnedBy {
-    Class<?> value() default Void.class;
+    Class<?> value();
 }
