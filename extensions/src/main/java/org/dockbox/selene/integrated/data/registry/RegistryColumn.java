1b3ed7d3c0dc4fc3ebc0a4e6918e32f718b87f2a
Guus Lieben <guus@xendox.com>
2020-12-30 11:55:13 +0100

    Delegate list in RegistryColumn

--- extensions/src/main/java/org/dockbox/selene/integrated/data/registry/RegistryColumn.java
+++ extensions/src/main/java/org/dockbox/selene/integrated/data/registry/RegistryColumn.java
@@ -17,21 +17,35 @@
 
 package org.dockbox.selene.integrated.data.registry;
 
+import org.dockbox.selene.core.annotations.Rejects;
+import org.dockbox.selene.core.impl.files.DefaultConfigurateManager;
 import org.dockbox.selene.core.objects.Exceptional;
+import org.dockbox.selene.core.util.SeleneUtils;
+import org.jetbrains.annotations.NotNull;
 
-import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Predicate;
 
-public class RegistryColumn<T> extends ArrayList<T> {
+@SuppressWarnings({"UnusedReturnValue", "unused"})
+@Rejects(DefaultConfigurateManager.class)
+public class RegistryColumn<T> {
+
+    private List<T> data = SeleneUtils.COLLECTION.emptyList();
 
     public RegistryColumn() {
         super();
     }
 
-    public RegistryColumn(Collection<? extends T> values) {
-        super(values);
+    public RegistryColumn(Collection<T> values) {
+        this.data = SeleneUtils.COLLECTION.asList(values);
+    }
+
+    public RegistryColumn(RegistryColumn<T> column) {
+        this.data = column.data;
     }
 
     /**
@@ -43,7 +57,7 @@ public class RegistryColumn<T> extends ArrayList<T> {
      * @return Itself
      */
     public RegistryColumn<T> removeValueIf(Predicate<? super T> filter) {
-        super.removeIf(filter);
+        this.data.removeIf(filter);
         return this;
     }
 
@@ -58,7 +72,7 @@ public class RegistryColumn<T> extends ArrayList<T> {
     public <K> RegistryColumn<K> mapTo(Function<? super T, K> mapper) {
         RegistryColumn<K> result = new RegistryColumn<>();
 
-        for (T value : this) {
+        for (T value : this.data) {
             result.add(mapper.apply(value));
         }
         return result;
@@ -75,7 +89,7 @@ public class RegistryColumn<T> extends ArrayList<T> {
     public <K> RegistryColumn<K> mapToSingleList(Function<? super T, ? extends Collection<K>> mapper) {
         RegistryColumn<K> result = new RegistryColumn<>();
 
-        for (T value : this) {
+        for (T value : this.data) {
             result.addAll(mapper.apply(value));
         }
         return result;
@@ -92,7 +106,7 @@ public class RegistryColumn<T> extends ArrayList<T> {
     public <K extends T> RegistryColumn<K> convertTo(Class<K> clazz) {
         RegistryColumn<K> result = new RegistryColumn<>();
 
-        for (T value : this) {
+        for (T value : this.data) {
             if (clazz.isInstance(value)) {
                 @SuppressWarnings("unchecked")
                 K convertedValue = (K)value;
@@ -111,7 +125,7 @@ public class RegistryColumn<T> extends ArrayList<T> {
      * @return An {@link Exceptional} containing the value of the first match, if one is found.
      */
     public Exceptional<T> firstMatch(Predicate<? super T> predicate) {
-        for (T value : this) {
+        for (T value : this.data) {
             if (predicate.test(value)) return Exceptional.of(value);
         }
         return Exceptional.empty();
@@ -124,7 +138,7 @@ public class RegistryColumn<T> extends ArrayList<T> {
      * @return An {@link Exceptional} containing the element at the provided index in the RegistryColumn, if one is found.
      */
     public Exceptional<T> getSafely(int index) {
-        return Exceptional.of(() -> super.get(index));
+        return Exceptional.of(() -> this.data.get(index));
     }
 
     /**
@@ -135,5 +149,26 @@ public class RegistryColumn<T> extends ArrayList<T> {
     public Exceptional<T> first() {
         return this.getSafely(0);
     }
+
+    public int size() {return this.data.size();}
+
+    public boolean contains(Object o) {return this.data.contains(o);}
+
+    public Iterator<T> iterator() {return this.data.iterator();}
+
+    public boolean add(T t) {return this.data.add(t);}
+
+    public boolean addAll(int index, @NotNull Collection<? extends T> c) {return this.data.addAll(index, c);}
+
+    public boolean addAll(int index, @NotNull RegistryColumn<? extends T> c) {return this.data.addAll(index, c.data);}
+
+    public boolean addAll(@NotNull Collection<? extends T> c) {return this.data.addAll(c);}
+
+    public boolean addAll(@NotNull RegistryColumn<? extends T> c) {return this.data.addAll(c.data);}
+
+    public T get(int index) {return this.data.get(index);}
+
+    public void forEach(Consumer<? super T> action) {this.data.forEach(action);}
+
 }
 
