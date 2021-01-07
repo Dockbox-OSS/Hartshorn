d9ff224379138cd8a527237eb7fedcbc345b9860
Guus Lieben <guus@xendox.com>
2021-01-07 20:45:34 +0100

    Implement index based slot lookup

--- sponge-7/src/main/java/org/dockbox/selene/sponge/objects/inventory/SpongePlayerInventory.java
+++ sponge-7/src/main/java/org/dockbox/selene/sponge/objects/inventory/SpongePlayerInventory.java
@@ -1,18 +1,18 @@
 /*
- *  Copyright (C) 2020 Guus Lieben
+ * Copyright (C) 2020 Guus Lieben
  *
- *  This framework is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU Lesser General Public License as
- *  published by the Free Software Foundation, either version 2.1 of the
- *  License, or (at your option) any later version.
+ * This framework is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 2.1 of the
+ * License, or (at your option) any later version.
  *
- *  This library is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See
- *  the GNU Lesser General Public License for more details.
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See
+ * the GNU Lesser General Public License for more details.
  *
- *  You should have received a copy of the GNU Lesser General Public License
- *  along with this library. If not, see {@literal<http://www.gnu.org/licenses/>}.
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library. If not, see {@literal<http://www.gnu.org/licenses/>}.
  */
 
 package org.dockbox.selene.sponge.objects.inventory;
@@ -30,12 +30,26 @@ import org.dockbox.selene.sponge.util.SpongeConversionUtil;
 import org.spongepowered.api.Sponge;
 import org.spongepowered.api.entity.living.player.Player;
 import org.spongepowered.api.item.inventory.ItemStack;
+import org.spongepowered.api.item.inventory.entity.Hotbar;
+import org.spongepowered.api.item.inventory.entity.MainPlayerInventory;
 import org.spongepowered.api.item.inventory.equipment.EquipmentType;
 import org.spongepowered.api.item.inventory.equipment.EquipmentTypes;
+import org.spongepowered.api.item.inventory.property.SlotIndex;
+import org.spongepowered.common.item.inventory.query.operation.InventoryTypeQueryOperation;
 
 import java.util.UUID;
+import java.util.function.Function;
+import java.util.function.Supplier;
 
 public class SpongePlayerInventory extends PlayerInventory {
+
+    private static final Supplier<Item> air = () -> Selene.getItems().getAir();
+    private static final Function<org.spongepowered.api.item.inventory.Slot, Item> slotLookup = slot -> {
+        return slot.peek().map(SpongeConversionUtil::fromSponge)
+            .map(referencedItem -> (Item) referencedItem)
+
+            .orElseGet(air);
+    };
 
     private final UUID playerUniqueId;
 
@@ -45,18 +59,48 @@ public class SpongePlayerInventory extends PlayerInventory {
 
     @Override
     public Item getSlot(int row, int column) {
-        return getPlayer().map(player -> {
+        return this.getPlayer().map(player -> {
+            if (3 > row) { // Main inventory
+                MainPlayerInventory main = player.getInventory()
+                    .query(new InventoryTypeQueryOperation(MainPlayerInventory.class));
 
-            return Item.of("");
-        }).orElse(Selene.getItems().getAir());
+                return main.getGrid()
+                    .getSlot(column, row)
+                    .map(slotLookup)
+                    .orElseGet(air);
+
+            } else if (3 == row) { // Hotbar
+                Hotbar hotbar = player.getInventory()
+                    .query(new InventoryTypeQueryOperation(Hotbar.class));
+                return hotbar.getSlot(new SlotIndex(column))
+                    .map(slotLookup)
+                    .orElseGet(air);
+
+            } else throw new IllegalArgumentException("Slot index [row="+row+", col=" + column +"] is out of bounds (row: 0-3, col: 0-8)");
+        }).orElseGet(air);
     }
 
+    @SuppressWarnings("MagicNumber")
     @Override
     public Item getSlot(int index) {
-        return getPlayer().map(player -> {
+        // Supplier prevents additional performance hit
+        final Supplier<Item> air = () -> Selene.getItems().getAir();
+        return this.getPlayer().map(player -> {
+            if (27 > index) { // Main inventory
+                MainPlayerInventory main = player.getInventory()
+                    .query(new InventoryTypeQueryOperation(MainPlayerInventory.class));
 
-            return Item.of("");
-        }).orElse(Selene.getItems().getAir());
+                return main.getGrid().getSlot(new SlotIndex(index)).map(slotLookup).orElseGet(air);
+
+            } else if (36 > index) { // Hotbar
+                Hotbar hotbar = player.getInventory()
+                    .query(new InventoryTypeQueryOperation(Hotbar.class));
+                // -27 to correct for the grid gap (main grid is excluded once we get the Hotbar inventory, and is 3x9 slots)
+                return hotbar.getSlot(new SlotIndex(index - 27))
+                    .map(slotLookup)
+                    .orElseGet(air);
+            } else throw new IllegalArgumentException("Slot index " + index + " is out of bounds (0-35)");
+        }).orElseGet(air);
     }
 
     @Override
