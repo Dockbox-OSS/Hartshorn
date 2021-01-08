8c3319f7ebeca6408b5dbf2452a3f98a1dcc20c0
Guus Lieben <guus@xendox.com>
2021-01-08 17:38:42 +0100

    Add 1.12 NMS project (isolated due to Gradle Wrapper 4.9 incompat)

---
+++ selene-nms-1.12/src/main/java/org/dockbox/selene/nms/packets/NMSSpawnEntityPacket.java
@@ -0,0 +1,37 @@
+/*
+ *  Copyright (C) 2020 Guus Lieben
+ *
+ *  This framework is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as
+ *  published by the Free Software Foundation, either version 2.1 of the
+ *  License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See
+ *  the GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this library. If not, see {@literal<http://www.gnu.org/licenses/>}.
+ */
+
+package org.dockbox.selene.nms.packets;
+
+import net.minecraft.network.play.server.SPacketSpawnGlobalEntity;
+
+import org.dockbox.selene.core.entities.Entity;
+import org.dockbox.selene.core.packets.SpawnEntityPacket;
+import org.dockbox.selene.nms.entities.NMSEntity;
+
+public class NMSSpawnEntityPacket<T extends Entity> extends SpawnEntityPacket<T> implements NMSPacket<SPacketSpawnGlobalEntity> {
+
+    @Override
+    public SPacketSpawnGlobalEntity getPacket() {
+        Entity entity = this.getEntity();
+        if (entity instanceof NMSEntity) {
+            return new SPacketSpawnGlobalEntity(((NMSEntity) entity).getEntity());
+        } else {
+            throw new UnsupportedOperationException("Cannot convert entity '" + entity + "' to SpongeEntity");
+        }
+    }
+}
