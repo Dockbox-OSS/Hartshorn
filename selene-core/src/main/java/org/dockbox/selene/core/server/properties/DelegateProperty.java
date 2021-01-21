76a82d67b99e50ea6cfc62b005c8b3a04c6ff068
Guus Lieben <guus@xendox.com>
2021-01-21 17:48:32 +0100

    Implement delegation functions

--- selene-core/src/main/java/org/dockbox/selene/core/server/properties/DelegateProperty.java
+++ selene-core/src/main/java/org/dockbox/selene/core/server/properties/DelegateProperty.java
@@ -1,5 +1,9 @@
 package org.dockbox.selene.core.server.properties;
 
+import org.dockbox.selene.core.delegate.DelegateTarget;
+import org.dockbox.selene.core.delegate.DelegationFunction;
+import org.dockbox.selene.core.delegate.DelegationHolder;
+
 import java.lang.reflect.Method;
 import java.util.function.BiConsumer;
 import java.util.function.BiFunction;
@@ -9,9 +13,12 @@ public final class DelegateProperty<T, R> implements InjectorProperty<Class<T>>{
     public static final String KEY = "SeleneInternalDelegateKey";
     private final Class<T> type;
     private final Method target;
-    private final BiFunction<T, Object[], R> delegate;
+    private final DelegationFunction<T, R> delegate;
+    private DelegateTarget delegateTarget = DelegateTarget.OVERWRITE;
+    private final DelegationHolder holder = new DelegationHolder();
+    private boolean overwriteResult = true;
 
-    private DelegateProperty(Class<T> type, Method target, BiFunction<T, Object[], R> delegate) {
+    private DelegateProperty(Class<T> type, Method target, DelegationFunction<T, R> delegate) {
         this.type = type;
         this.target = target;
         this.delegate = delegate;
@@ -27,23 +34,48 @@ public final class DelegateProperty<T, R> implements InjectorProperty<Class<T>>{
         return this.type;
     }
 
-    public BiFunction<T, Object[], R> getDelegate() {
-        return this.delegate;
+    public DelegateTarget getTarget() {
+        return this.delegateTarget;
     }
 
-    public Method getTarget() {
+    public void setTarget(DelegateTarget delegateTarget) {
+        this.delegateTarget = delegateTarget;
+    }
+
+    public R delegate(T instance, Object... args) {
+        this.holder.setCancelled(false);
+        return this.delegate.delegate(instance, args, this.holder);
+    }
+
+    public Method getTargetMethod() {
         return this.target;
     }
 
+    public boolean isCancelled() {
+        return this.holder.isCancelled();
+    }
+
+    public boolean overwriteResult() {
+        return this.overwriteResult;
+    }
+
+    public void setOverwriteResult(boolean overwriteResult) {
+        this.overwriteResult = overwriteResult;
+    }
+
     public static <T, R> DelegateProperty<T, R> of(Class<T> type, Method target, BiFunction<T, Object[], R> delegate) {
-        return new DelegateProperty<>(type, target, delegate);
+        return new DelegateProperty<>(type, target, (instance, args, holder) -> delegate.apply(instance, args));
     }
 
     public static <T, R> DelegateProperty<T, R> of(Class<T> type, Method target, BiConsumer<T, Object[]> delegate) {
-        return new DelegateProperty<>(type, target, (instance, args) -> {
+        return new DelegateProperty<>(type, target, (instance, args, holder) -> {
             delegate.accept(instance, args);
             //noinspection ReturnOfNull
             return null;
         });
+    }
+
+    public static <T, R> DelegateProperty<T, R> of(Class<T> type, Method target, DelegationFunction<T, R> delegate) {
+        return new DelegateProperty<>(type, target, delegate);
     }
 }
