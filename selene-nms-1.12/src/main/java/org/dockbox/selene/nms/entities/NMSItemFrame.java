58d514b85393195f847f3ee7f4a2c5d3003d5937
Guus Lieben <guus@xendox.com>
2021-02-02 15:04:47 +0100

    Remove NMS entity targets

--- selene-nms-1.12/src/main/java/org/dockbox/selene/nms/entities/NMSItemFrame.java
+++
@@ -1,9 +0,0 @@
-package org.dockbox.selene.nms.entities;
-
-import net.minecraft.entity.item.EntityItemFrame;
-
-import org.dockbox.selene.core.entities.ItemFrame;
-
-public abstract class NMSItemFrame extends NMSEntity<EntityItemFrame> implements ItemFrame {
-
-}
