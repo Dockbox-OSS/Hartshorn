d32e244f0ff32a1524de8dd6f2269fbb2c369743
Guus Lieben <guus@xendox.com>
2021-04-21 20:15:28 +0200

    Add tests for Compiler module

---
+++ modules/compiler/src/test/java/org/dockbox/selene/compiler/User.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright (C) 2020 Guus Lieben
+ *
+ * This framework is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 2.1 of the
+ * License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See
+ * the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library. If not, see {@literal<http://www.gnu.org/licenses/>}.
+ */
+
+package org.dockbox.selene.compiler;
+
+/**
+ * Sample class as JavaBean.
+ * 
+ * @author michael
+ */
+public class User {
+
+	private String id;
+	private String name;
+	private long created;
+
+	public String getId() {
+		return this.id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public long getCreated() {
+		return this.created;
+	}
+
+	public void setCreated(long created) {
+		this.created = created;
+	}
+
+}
