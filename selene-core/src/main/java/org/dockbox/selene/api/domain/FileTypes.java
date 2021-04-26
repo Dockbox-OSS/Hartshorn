e2c50874ba2a80a52843fc7148a27728ca360ce5
Guus Lieben <guus@xendox.com>
2021-04-26 19:16:39 +0200

    Move default file types to core

--- selene-core/src/main/java/org/dockbox/selene/api/domain/FileTypes.java
+++ selene-core/src/main/java/org/dockbox/selene/api/domain/FileTypes.java
@@ -15,7 +15,7 @@
  * along with this library. If not, see {@literal<http://www.gnu.org/licenses/>}.
  */
 
-package org.dockbox.selene.persistence;
+package org.dockbox.selene.api.domain;
 
 public class FileTypes {
 
