3e691b4300ed1506c44e301c920ddebb4f9e66e7
Guus Lieben <guus@xendox.com>
2021-04-27 23:03:44 +0200

    Migrate world management resources

--- selene-minecraft-modules/worldmanagement/src/main/java/org/dockbox/selene/worldmanagement/WorldManagementResources.java
+++ selene-minecraft-modules/worldmanagement/src/main/java/org/dockbox/selene/worldmanagement/WorldManagementResources.java
@@ -17,16 +17,17 @@
 
 package org.dockbox.selene.worldmanagement;
 
+import org.dockbox.selene.api.i18n.annotations.Resource;
 import org.dockbox.selene.api.i18n.annotations.Resources;
 import org.dockbox.selene.api.i18n.common.ResourceEntry;
-import org.dockbox.selene.api.i18n.entry.Resource;
 
 @Resources(WorldManagement.class)
-public class WorldManagementResources {
+public interface WorldManagementResources {
 
-    public static final ResourceEntry WORLD_BLACKLIST_ADDED = new Resource("$2{0} $1was added to the blacklist and will not be unloaded", "worldmanagement.unloader.blacklist.added");
-    public static final ResourceEntry WORLD_BLACKLIST_FAILED = new Resource("$2{0} $1could not be blacklisted, are you sure it exists?", "worldmanagement.unloader.blacklist.failed");
+    @Resource(value = "$2{0} $1was added to the blacklist and will not be unloaded", key = "worldmanagement.unloader.blacklist.added")
+    ResourceEntry getBlacklistAdded(String world);
 
-    public static final String WORLD_MANAGER = "selene.worlds";
+    @Resource(value = "$2{0} $1could not be blacklisted, are you sure it exists?", key = "worldmanagement.unloader.blacklist.failed")
+    ResourceEntry getBlacklistFailure(String world);
 
 }
