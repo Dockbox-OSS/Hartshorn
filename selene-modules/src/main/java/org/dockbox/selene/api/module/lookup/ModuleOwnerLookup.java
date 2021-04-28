df2fbaa1ded26255117cd0622fa42cea7e22864d
Guus Lieben <guus@xendox.com>
2021-04-28 14:25:54 +0200

    Lookup module owner from annotation if container is absent

--- selene-modules/src/main/java/org/dockbox/selene/api/module/lookup/ModuleOwnerLookup.java
+++ selene-modules/src/main/java/org/dockbox/selene/api/module/lookup/ModuleOwnerLookup.java
@@ -15,16 +15,28 @@
  * along with this library. If not, see {@literal<http://www.gnu.org/licenses/>}.
  */
 
-package org.dockbox.selene.api.module;
+package org.dockbox.selene.api.module.lookup;
 
+import org.dockbox.selene.api.domain.Exceptional;
 import org.dockbox.selene.api.domain.OwnerLookup;
+import org.dockbox.selene.api.domain.SimpleTypedOwner;
 import org.dockbox.selene.api.domain.TypedOwner;
+import org.dockbox.selene.api.module.Modules;
+import org.dockbox.selene.api.module.annotations.Module;
 import org.dockbox.selene.di.annotations.Binds;
 
 @Binds(OwnerLookup.class)
 public class ModuleOwnerLookup implements OwnerLookup {
     @Override
     public TypedOwner lookup(Class<?> type) {
-        return Modules.module(type);
+        return Exceptional.of(Modules.module(type))
+                .map(TypedOwner.class::cast)
+                .then(() -> {
+                    // In case modules have not been scanned yet, typically only caused by preloads accessing headers
+                    if (type.isAnnotationPresent(Module.class)) {
+                        return SimpleTypedOwner.of(type.getAnnotation(Module.class).id());
+                    }
+                    return null;
+                }).orNull();
     }
 }
