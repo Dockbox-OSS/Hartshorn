69dcf562c44d19da806fbc579ede4fef3265d6f4
Guus Lieben <guus@xendox.com>
2021-06-21 16:17:14 +0200

    Register services automatically

---
+++ hartshorn-commands/src/main/java/org/dockbox/hartshorn/commands/service/CommandServiceScanner.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (C) 2020 Guus Lieben
+ *
+ * This framework is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 2.1 of the
+ * License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See
+ * the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library. If not, see {@literal<http://www.gnu.org/licenses/>}.
+ */
+
+package org.dockbox.hartshorn.commands.service;
+
+import org.dockbox.hartshorn.api.Hartshorn;
+import org.dockbox.hartshorn.api.annotations.PostBootstrap;
+import org.dockbox.hartshorn.api.annotations.UseBootstrap;
+import org.dockbox.hartshorn.commands.CommandGateway;
+import org.dockbox.hartshorn.commands.annotations.Command;
+import org.dockbox.hartshorn.di.annotations.Service;
+import org.dockbox.hartshorn.di.preload.Preloadable;
+import org.dockbox.hartshorn.di.services.ServiceContainer;
+import org.dockbox.hartshorn.util.Reflect;
+
+@Service(activator = UseBootstrap.class)
+public class CommandServiceScanner implements Preloadable {
+
+    @PostBootstrap
+    public void preload() {
+        final CommandGateway gateway = Hartshorn.context().get(CommandGateway.class);
+        for (ServiceContainer container : Hartshorn.context().locator().containers()) {
+            if (!Reflect.annotatedMethods(container.getType(), Command.class).isEmpty()) {
+                gateway.register(container.getType());
+            }
+        }
+    }
+
+}
