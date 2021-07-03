b02b3a0249d1bbe70f459e2d21aad6f24a80e788
Guus Lieben <guus@xendox.com>
2021-07-03 20:39:36 +0200

    Draft: Entity implementation

---
+++ hartshorn-sponge-8/src/main/java/org/dockbox/hartshorn/sponge/game/SpongeGenericEntity.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (C) 2020 Guus Lieben
+ *
+ * This framework is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 2.1 of the
+ * License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See
+ * the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library. If not, see {@literal<http://www.gnu.org/licenses/>}.
+ */
+
+package org.dockbox.hartshorn.sponge.game;
+
+import net.minecraft.world.entity.Entity;
+
+import org.dockbox.hartshorn.api.domain.Exceptional;
+import org.dockbox.hartshorn.sponge.game.entity.SpongeEntity;
+import org.spongepowered.api.entity.EntityType;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class SpongeGenericEntity implements SpongeEntity<Entity, org.spongepowered.api.entity.Entity> {
+
+    private final org.spongepowered.api.entity.Entity entity;
+
+    @Override
+    public EntityType<org.spongepowered.api.entity.Entity> type() {
+        //noinspection unchecked
+        return (EntityType<org.spongepowered.api.entity.Entity>) this.entity.type();
+    }
+
+    @Override
+    public Exceptional<org.spongepowered.api.entity.Entity> spongeEntity() {
+        return Exceptional.of(this.entity);
+    }
+}
