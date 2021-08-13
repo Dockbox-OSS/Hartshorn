1a83742d25efac49a47d06c188fe77c8a6f08b7e
Guus Lieben <guus@xendox.com>
2021-08-13 11:26:14 +0200

    #369 Initial paginated pane builder draft

---
+++ hartshorn-sponge-8/src/main/java/org/dockbox/hartshorn/sponge/inventory/panes/SpongePaginatedPane.java
@@ -0,0 +1,26 @@
+package org.dockbox.hartshorn.sponge.inventory.panes;
+
+import org.dockbox.hartshorn.server.minecraft.inventory.Element;
+import org.dockbox.hartshorn.server.minecraft.inventory.pane.PaginatedPane;
+import org.dockbox.hartshorn.server.minecraft.players.Player;
+
+import java.util.Collection;
+
+// TODO: #369 Implement this
+public class SpongePaginatedPane implements PaginatedPane {
+
+    @Override
+    public void open(final Player player, final int page) {
+
+    }
+
+    @Override
+    public void elements(final Collection<Element> elements) {
+
+    }
+
+    @Override
+    public void open(final Player player) {
+
+    }
+}
---
+++ hartshorn-sponge-8/src/main/java/org/dockbox/hartshorn/sponge/inventory/panes/SpongePaginatedPaneBuilder.java
@@ -0,0 +1,26 @@
+package org.dockbox.hartshorn.sponge.inventory.panes;
+
+import org.dockbox.hartshorn.i18n.text.Text;
+import org.dockbox.hartshorn.server.minecraft.inventory.Element;
+import org.dockbox.hartshorn.server.minecraft.inventory.builder.PaginatedPaneBuilder;
+import org.dockbox.hartshorn.server.minecraft.inventory.pane.PaginatedPane;
+
+import java.util.Collection;
+
+// TODO: #369 Implement this
+public class SpongePaginatedPaneBuilder extends PaginatedPaneBuilder {
+    @Override
+    public PaginatedPaneBuilder elements(Collection<Element> elements) {
+        return null;
+    }
+
+    @Override
+    public PaginatedPaneBuilder title(Text text) {
+        return null;
+    }
+
+    @Override
+    public PaginatedPane build() {
+        return null;
+    }
+}
