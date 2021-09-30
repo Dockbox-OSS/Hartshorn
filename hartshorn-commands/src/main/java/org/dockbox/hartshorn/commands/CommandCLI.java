ecde7f2efa1fb386cc9e00991f8215fe5e00efb5
Guus Lieben <guus@xendox.com>
2021-09-30 16:54:47 +0200

    #406 Add CommandCLI startup documentation

--- hartshorn-commands/src/main/java/org/dockbox/hartshorn/commands/CommandCLI.java
+++ hartshorn-commands/src/main/java/org/dockbox/hartshorn/commands/CommandCLI.java
@@ -4,7 +4,18 @@ import java.io.InputStream;
 
 /**
  * Represents a constant CLI which is capable of listening to command inputs. Commands may be entered through
- * any mean, like a command line, external event bus, or similar solutions.
+ * any mean, like a command line, external event bus, or similar solutions. Should be activated after the engine
+ * started, typically this can be done by listening for {@link org.dockbox.hartshorn.boot.EngineChangedState} with
+ * {@link org.dockbox.hartshorn.boot.ServerState.Started} as its parameter.
+ *
+ * <p>For example
+ * <pre>{@code
+ * @Listener
+ * public void on(EngineChangedState<Started> event) {
+ *      event.applicationContext().get(CommandCLI.class).open();
+ * }
+ * }</pre>
+ */
 public interface CommandCLI {
     void open();
 
