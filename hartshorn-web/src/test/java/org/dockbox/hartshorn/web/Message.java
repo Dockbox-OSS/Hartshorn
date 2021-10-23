d0b9d58cc5a3d2c4f6aeaa066f3ee0fe7a3e2a20
Guus Lieben <guus@xendox.com>
2021-10-23 17:51:50 +0200

    #417 Test body and header argument processors

---
+++ hartshorn-web/src/test/java/org/dockbox/hartshorn/web/Message.java
@@ -0,0 +1,12 @@
+package org.dockbox.hartshorn.web;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class Message {
+    private String message;
+}
