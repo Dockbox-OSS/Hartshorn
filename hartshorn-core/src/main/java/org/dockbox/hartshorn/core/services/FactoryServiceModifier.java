20fba9616497a486d5cf83ae731c2619b773c0a2
Guus Lieben <guus@xendox.com>
2021-11-08 11:36:32 +0100

    #473 Populate and enable factory bound type creation

--- hartshorn-core/src/main/java/org/dockbox/hartshorn/core/services/FactoryServiceModifier.java
+++ hartshorn-core/src/main/java/org/dockbox/hartshorn/core/services/FactoryServiceModifier.java
@@ -2,11 +2,13 @@ package org.dockbox.hartshorn.core.services;
 
 import org.dockbox.hartshorn.core.annotations.Factory;
 import org.dockbox.hartshorn.core.annotations.service.Service;
+import org.dockbox.hartshorn.core.binding.Bindings;
 import org.dockbox.hartshorn.core.context.ApplicationContext;
 import org.dockbox.hartshorn.core.context.FactoryContext;
 import org.dockbox.hartshorn.core.context.MethodProxyContext;
 import org.dockbox.hartshorn.core.context.element.ConstructorContext;
 import org.dockbox.hartshorn.core.context.element.MethodContext;
+import org.dockbox.hartshorn.core.exceptions.ApplicationException;
 import org.dockbox.hartshorn.core.proxy.ProxyFunction;
 
 public class FactoryServiceModifier extends ServiceAnnotatedMethodModifier<Factory, Service> {
@@ -28,14 +30,24 @@ public class FactoryServiceModifier extends ServiceAnnotatedMethodModifier<Facto
 
     @Override
     public <T, R> ProxyFunction<T, R> process(final ApplicationContext context, final MethodProxyContext<T> methodContext) {
-        if (methodContext.method().isAbstract()) {
-            final MethodContext<?, T> method = methodContext.method();
+        final MethodContext<?, T> method = methodContext.method();
+        if (method.isAbstract()) {
             FactoryContext factoryContext = context.first(FactoryContext.class).get();
             final ConstructorContext<?> constructor = factoryContext.get(method);
-            return (instance, args, proxyContext) -> (R) constructor.createInstance(args).orNull();
+            return (instance, args, proxyContext) -> this.populateAndEnable(context, (R) constructor.createInstance(args).orNull());
         }
         else {
-            return (instance, args, proxyContext) -> (R) methodContext.method().invoke(instance, args);
+            return (instance, args, proxyContext) -> this.populateAndEnable(context, (R) methodContext.method().invoke(instance, args).orNull());
+        }
+    }
+
+    private <T> T populateAndEnable(final ApplicationContext context, final T instance) {
+        try {
+            context.populate(instance);
+            Bindings.enable(instance);
+            return instance;
+        } catch (ApplicationException e) {
+            throw e.runtime();
         }
     }
 
