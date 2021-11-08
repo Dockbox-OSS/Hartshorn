8a2f978ac493cb1858eee7a4ba53b0f34dca031c
Guus Lieben <guus@xendox.com>
2021-11-08 08:34:00 +0100

    #355 Fix missing license headers

--- hartshorn-core/src/main/java/org/dockbox/hartshorn/core/proxy/JavaInterfaceProxyHandler.java
+++ hartshorn-core/src/main/java/org/dockbox/hartshorn/core/proxy/JavaInterfaceProxyHandler.java
@@ -12,8 +12,7 @@
  * the GNU Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public License
- * along with this library. If not, see {
-@literal<http://www.gnu.org/licenses/>}.
+ * along with this library. If not, see {@literal<http://www.gnu.org/licenses/>}.
  */
 
 package org.dockbox.hartshorn.core.proxy;
