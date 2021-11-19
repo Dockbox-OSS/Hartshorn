acb73f1a303d0fb3e85433c0d0e5068a237224d6
Guus Lieben <guus@xendox.com>
2021-11-19 10:57:21 +0100

    #507 Final `Key` framework support removal

--- examples/src/caching/java/org/dockbox/hartshorn/demo/caching/domain/User.java
+++ examples/src/caching/java/org/dockbox/hartshorn/demo/caching/domain/User.java
@@ -17,23 +17,16 @@
 
 package org.dockbox.hartshorn.demo.caching.domain;
 
-import org.dockbox.hartshorn.core.keys.KeyHolder;
-import org.dockbox.hartshorn.core.context.ApplicationContext;
-
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 /**
- * A simple bean type which can carry {@link org.dockbox.hartshorn.core.keys.Key keys}, as it implements
- * {@link KeyHolder}. {@link org.dockbox.hartshorn.core.keys.Key Keys} are not persistent, and depend on
- * a managed data store.
+ * A simple bean type which represents a basic user with a name and age.
  */
 @Getter
 @AllArgsConstructor
-public class User implements KeyHolder<User> {
+public class User {
 
-    // For the KeyHolder
-    private final ApplicationContext applicationContext;
     private final String name;
     private final int age;
 }
