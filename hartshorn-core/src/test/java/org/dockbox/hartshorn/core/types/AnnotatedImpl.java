2ced6b0b92f639fec4dad1dbee1fc7875db8d23d
Guus Lieben <guus@xendox.com>
2021-12-27 16:49:42 +0100

    #621 Validate that redefined annotations take priority over parents

---
+++ hartshorn-core/src/test/java/org/dockbox/hartshorn/core/types/AnnotatedImpl.java
@@ -0,0 +1,7 @@
+package org.dockbox.hartshorn.core.types;
+
+import org.dockbox.hartshorn.core.annotations.Base;
+
+@Base("impl")
+public class AnnotatedImpl implements AnnotatedParent {
+}
---
+++ hartshorn-core/src/test/java/org/dockbox/hartshorn/core/types/AnnotatedParent.java
@@ -0,0 +1,7 @@
+package org.dockbox.hartshorn.core.types;
+
+import org.dockbox.hartshorn.core.annotations.Base;
+
+@Base("parent")
+public interface AnnotatedParent {
+}
