2a58c9dfd8e43182d4698a7a0d887f7d66ac71ff
Guus Lieben <GLieben@costengineering.eu>
2022-06-02 12:14:41 +0200

    #747 Validate class and activator condition matching

--- hartshorn-core/src/test/java/org/dockbox/hartshorn/core/conditions/ConditionTests.java
+++ hartshorn-core/src/test/java/org/dockbox/hartshorn/core/conditions/ConditionTests.java
@@ -18,16 +18,28 @@ package org.dockbox.hartshorn.core.conditions;
 
 import org.dockbox.hartshorn.application.ApplicationFactory;
 import org.dockbox.hartshorn.application.context.ApplicationContext;
+import org.dockbox.hartshorn.component.condition.ActivatorCondition;
+import org.dockbox.hartshorn.component.condition.ClassCondition;
+import org.dockbox.hartshorn.component.condition.Condition;
+import org.dockbox.hartshorn.component.condition.ConditionContext;
+import org.dockbox.hartshorn.component.condition.ConditionResult;
+import org.dockbox.hartshorn.component.condition.RequiresActivator;
+import org.dockbox.hartshorn.component.condition.RequiresClass;
+import org.dockbox.hartshorn.component.condition.RequiresCondition;
 import org.dockbox.hartshorn.inject.Key;
 import org.dockbox.hartshorn.inject.binding.BindingHierarchy;
 import org.dockbox.hartshorn.testsuite.HartshornFactory;
 import org.dockbox.hartshorn.testsuite.HartshornTest;
 import org.dockbox.hartshorn.testsuite.TestComponents;
+import org.dockbox.hartshorn.util.reflect.MethodContext;
+import org.dockbox.hartshorn.util.reflect.TypeContext;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
+import java.lang.annotation.Annotation;
 import java.util.stream.Stream;
 
 import jakarta.inject.Inject;
@@ -43,10 +55,16 @@ public class ConditionTests {
     public static ApplicationFactory<?, ?> factory(final ApplicationFactory<?, ?> factory) {
         return factory.arguments("--property.c=o",
                 "--property.d=d",
-                "--property.e=otherValue");
+                "--property.e=otherValue")
+                .serviceActivator(new DemoActivator() {
+                    @Override
+                    public Class<? extends Annotation> annotationType() {
+                        return DemoActivator.class;
+                    }
+                });
     }
 
-    public static Stream<Arguments> providers() {
+    public static Stream<Arguments> properties() {
         return Stream.of(
                 Arguments.of("a", true),
                 Arguments.of("b", false),
@@ -58,8 +76,8 @@ public class ConditionTests {
     }
 
     @ParameterizedTest
-    @MethodSource("providers")
-    void test(final String name, final boolean present) {
+    @MethodSource("properties")
+    void testPropertyConditions(final String name, final boolean present) {
         final Key<String> key = Key.of(String.class, name);
         final BindingHierarchy<String> hierarchy = this.applicationContext.hierarchy(key);
         Assertions.assertEquals(present ? 1 : 0, hierarchy.size());
@@ -72,4 +90,43 @@ public class ConditionTests {
             Assertions.assertEquals("", value); // Default value, not null
         }
     }
+
+    @Test
+    void testActivatorConditions() throws NoSuchMethodException {
+        Assertions.assertTrue(this.applicationContext.hasActivator(DemoActivator.class));
+
+        final MethodContext<?, ?> method = MethodContext.of(ConditionTests.class.getDeclaredMethod("requiresActivator"));
+        final RequiresCondition annotation = method.annotation(RequiresCondition.class).get();
+
+        final ConditionContext context = new ConditionContext(this.applicationContext, method, annotation);
+        final Condition condition = new ActivatorCondition();
+
+        final ConditionResult result = condition.matches(context);
+        Assertions.assertTrue(result.matches());
+    }
+
+    @RequiresActivator(DemoActivator.class)
+    private void requiresActivator() {}
+
+    @Test
+    void testClassConditions() {
+        final TypeContext<ConditionTests> type = TypeContext.of(ConditionTests.class);
+        final Condition condition = new ClassCondition();
+
+        final MethodContext<?, ConditionTests> requiresClass = type.method("requiresClass").get();
+        final RequiresCondition annotationForPresent = requiresClass.annotation(RequiresCondition.class).get();
+        final ConditionContext contextForPresent = new ConditionContext(this.applicationContext, requiresClass, annotationForPresent);
+        Assertions.assertTrue(condition.matches(contextForPresent).matches());
+
+        final MethodContext<?, ConditionTests> requiresAbsentClass = type.method("requiresAbsentClass").get();
+        final RequiresCondition annotationForAbsent = requiresAbsentClass.annotation(RequiresCondition.class).get();
+        final ConditionContext contextForAbsent = new ConditionContext(this.applicationContext, requiresAbsentClass, annotationForAbsent);
+        Assertions.assertFalse(condition.matches(contextForAbsent).matches());
+    }
+    @RequiresClass("java.lang.String")
+    private void requiresClass() {}
+
+    @RequiresClass("java.gnal.String")
+    private void requiresAbsentClass() {}
+
 }
