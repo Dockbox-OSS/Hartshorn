af0d60d7a342aa81d7b47dc788d082b6759c66ff
Guus Lieben <guus@xendox.com>
2022-06-29 20:01:46 +0200

    #770 Simplify standard system module

--- hartshorn-hsl/src/main/java/org/dockbox/hartshorn/hsl/modules/SystemLibrary.java
+++ hartshorn-hsl/src/main/java/org/dockbox/hartshorn/hsl/modules/SystemLibrary.java
@@ -2,16 +2,8 @@ package org.dockbox.hartshorn.hsl.modules;
 
 public class SystemLibrary {
 
-    public String osName() {
-        return System.getProperty("os.name");
-    }
-
-    public String osEnvVar(final String program) {
+    public String env(final String program) {
         return System.getenv(program);
-    }
-
-    public String osArch( ) {
-        return System.getProperty("os.arch");
     }
 }
 
