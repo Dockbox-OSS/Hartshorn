58993580a327ae98922af8346c1ae9367341c122
Guus Lieben <guus@xendox.com>
2022-06-29 20:00:10 +0200

    #770 Add lexer/tokenizing tests

---
+++ hartshorn-hsl/src/test/java/org/dockbox/hartshorn/hsl/LexerTests.java
@@ -0,0 +1,156 @@
+package org.dockbox.hartshorn.hsl;
+
+import org.dockbox.hartshorn.hsl.callable.ErrorReporter;
+import org.dockbox.hartshorn.hsl.lexer.Comment;
+import org.dockbox.hartshorn.hsl.lexer.Lexer;
+import org.dockbox.hartshorn.hsl.runtime.Phase;
+import org.dockbox.hartshorn.hsl.token.Token;
+import org.dockbox.hartshorn.hsl.token.TokenConstants;
+import org.dockbox.hartshorn.hsl.token.TokenType;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class LexerTests {
+
+    private static final TokenType[] keywords = {
+            TokenType.PREFIX, TokenType.INFIX,
+            TokenType.CLASS, TokenType.EXTENDS,
+            TokenType.IF, TokenType.ELSE,
+            TokenType.FUN, TokenType.RETURN, TokenType.NATIVE,
+            TokenType.TRUE, TokenType.FALSE,
+            TokenType.FOR, TokenType.DO, TokenType.WHILE, TokenType.REPEAT,
+            TokenType.BREAK, TokenType.CONTINUE,
+            TokenType.OR, TokenType.AND, TokenType.XOR,
+            TokenType.SUPER, TokenType.THIS,
+            TokenType.NULL, TokenType.ARRAY,
+            TokenType.VAR, TokenType.MODULE,
+            TokenType.PRINT, TokenType.TEST,
+    };
+
+    private static final List<TokenType> literals = List.of(
+            TokenType.IDENTIFIER,
+            TokenType.STRING,
+            TokenType.NUMBER,
+            TokenType.CHAR,
+            TokenType.EOF);
+
+    private static final TokenType[] singleTokens = {
+            TokenType.LEFT_PAREN, TokenType.RIGHT_PAREN,
+            TokenType.LEFT_BRACE, TokenType.RIGHT_BRACE,
+            TokenType.ARRAY_OPEN, TokenType.ARRAY_CLOSE,
+            TokenType.COMMA, TokenType.DOT,
+            TokenType.MINUS, TokenType.PLUS,
+            TokenType.SEMICOLON, TokenType.SLASH,
+            TokenType.STAR, TokenType.EQUAL, TokenType.BANG,
+            TokenType.GREATER, TokenType.LESS,
+            TokenType.QUESTION_MARK, TokenType.COLON,
+    };
+
+    public static Stream<Arguments> tokens() {
+        final List<Arguments> arguments = new ArrayList<>();
+        for (final TokenType type : TokenType.values()) {
+            if (literals.contains(type)) continue;
+            arguments.add(Arguments.of(type.representation(), type));
+        }
+        arguments.add(Arguments.of("12.0", TokenType.NUMBER));
+        arguments.add(Arguments.of("foobar", TokenType.IDENTIFIER));
+        arguments.add(Arguments.of("\"foo\"", TokenType.STRING));
+        arguments.add(Arguments.of("'a'", TokenType.CHAR));
+        return arguments.stream();
+    }
+
+    public static Stream<Arguments> keywords() {
+        return Arrays.stream(keywords).map(Arguments::of);
+    }
+
+    public static Stream<Arguments> singleCharacterTokens() {
+        return Arrays.stream(singleTokens).map(Arguments::of);
+    }
+
+    @ParameterizedTest
+    @MethodSource("singleCharacterTokens")
+    void testTokenTypesUseConstantsOfSameName(final TokenType type) throws IllegalAccessException, NoSuchFieldException {
+        final String name = type.name();
+        final Field field = TokenConstants.class.getField(name);
+        // Should be static and public
+        final char constant = (char) field.get(null);
+        Assertions.assertEquals("" + constant, type.representation());
+    }
+
+    @ParameterizedTest
+    @MethodSource("keywords")
+    void testKeywordsMatchExpectedList(final TokenType type) {
+        final Collection<TokenType> tokenTypes = TokenType.keywords().values();
+        Assertions.assertTrue(tokenTypes.contains(type));
+    }
+
+    @ParameterizedTest
+    @MethodSource("keywords")
+    void testKeywordsHaveCorrectMetaData(final TokenType type) {
+        Assertions.assertTrue(type.keyword());
+    }
+
+    @ParameterizedTest
+    @MethodSource("tokens")
+    void testCorrectToken(final String text, final TokenType expected) {
+        final Lexer lexer = new Lexer(text, this.errorReporter());
+        final List<Token> tokens = lexer.scanTokens();
+        Assertions.assertNotNull(tokens);
+        Assertions.assertEquals(2, tokens.size());
+        final Token token = tokens.get(0);
+        Assertions.assertEquals(expected, token.type());
+        Assertions.assertEquals(1, token.line());
+        final Token eof = tokens.get(1);
+        Assertions.assertEquals(TokenType.EOF, eof.type());
+    }
+
+    @Test
+    void testSingleLineComment() {
+        final Lexer lexer = new Lexer("# Comment", this.errorReporter());
+        final List<Token> tokens = lexer.scanTokens();
+
+        Assertions.assertNotNull(tokens);
+        Assertions.assertEquals(1, tokens.size());
+
+        final Token token = tokens.get(0);
+        Assertions.assertEquals(TokenType.EOF, token.type());
+
+        final List<Comment> comments = lexer.comments();
+        Assertions.assertNotNull(comments);
+        Assertions.assertEquals(1, comments.size());
+
+        final Comment comment = comments.get(0);
+        // Comments are not trimmed, include whitespace
+        Assertions.assertEquals(" Comment", comment.text());
+    }
+
+    ErrorReporter errorReporter() {
+        return new ErrorReporter() {
+            @Override
+            public void error(final Phase phase, final int line, final String message) {
+                Assertions.fail("Error reported: " + message);
+            }
+
+            @Override
+            public void error(final Phase phase, final Token token, final String message) {
+                Assertions.fail("Error reported: " + message);
+            }
+
+            @Override
+            public void clear() {
+                // Do nothing
+            }
+        };
+    }
+
+}
---
+++ hartshorn-hsl/src/test/java/org/dockbox/hartshorn/hsl/ScriptRuntimeTests.java
@@ -0,0 +1,212 @@
+package org.dockbox.hartshorn.hsl;
+
+import org.dockbox.hartshorn.application.context.ApplicationContext;
+import org.dockbox.hartshorn.hsl.callable.module.InstanceNativeModule;
+import org.dockbox.hartshorn.hsl.customizer.AbstractCodeCustomizer;
+import org.dockbox.hartshorn.hsl.customizer.CodeCustomizer;
+import org.dockbox.hartshorn.hsl.customizer.ScriptContext;
+import org.dockbox.hartshorn.hsl.lexer.Comment;
+import org.dockbox.hartshorn.hsl.runtime.Phase;
+import org.dockbox.hartshorn.hsl.runtime.StandardRuntime;
+import org.dockbox.hartshorn.hsl.runtime.ValidateExpressionRuntime;
+import org.dockbox.hartshorn.testsuite.HartshornTest;
+import org.dockbox.hartshorn.util.Result;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.stream.Stream;
+
+import jakarta.inject.Inject;
+
+@HartshornTest
+@UseExpressionValidation
+public class ScriptRuntimeTests {
+
+    @Inject
+    private ApplicationContext applicationContext;
+
+    public static Stream<Arguments> scripts() {
+        final Path resources = Paths.get("src", "test", "resources");
+        return Arrays.stream(resources.toFile().listFiles())
+                .filter(file -> file.getName().endsWith(".hsl"))
+                .map(file -> Result.of(() -> Files.readAllLines(file.toPath())).orNull())
+                .filter(Objects::nonNull)
+                .map(lines -> String.join("\n", lines))
+                .map(Arguments::of);
+    }
+
+    public static Stream<Arguments> phases() {
+        return Arrays.stream(Phase.values()).map(Arguments::of);
+    }
+
+    @ParameterizedTest
+    @MethodSource("scripts")
+    void testPredefinedScript(final String content) {
+        final StandardRuntime runtime = this.applicationContext.get(StandardRuntime.class);
+        final ScriptContext context = Assertions.assertDoesNotThrow(() -> runtime.run(content));
+
+        Assertions.assertTrue(context.errors().isEmpty());
+        Assertions.assertTrue(context.runtimeErrors().isEmpty());
+    }
+
+    @ParameterizedTest
+    @MethodSource("scripts")
+    void testPredefinedScriptWithOptionalSemicolons(final String content) {
+        final String source = content.replaceAll(";", "");
+        final StandardRuntime runtime = this.applicationContext.get(StandardRuntime.class);
+        final ScriptContext context = Assertions.assertDoesNotThrow(() -> runtime.run(source));
+
+        Assertions.assertTrue(context.errors().isEmpty());
+        Assertions.assertTrue(context.runtimeErrors().isEmpty());
+    }
+
+    @Test
+    void testExpression() {
+        final ValidateExpressionRuntime runtime = this.applicationContext.get(ValidateExpressionRuntime.class);
+        final String expression = "1 == 1";
+        final ScriptContext context = runtime.run(expression);
+
+        Assertions.assertTrue(context.errors().isEmpty());
+        Assertions.assertTrue(context.runtimeErrors().isEmpty());
+        Assertions.assertTrue(ValidateExpressionRuntime.valid(context));
+    }
+
+    @Test
+    void testComplexExpression() {
+        final ValidateExpressionRuntime runtime = this.applicationContext.get(ValidateExpressionRuntime.class);
+        final String expression = "1 + 3 == 4 and 2 + 2 == 4 and 2 - 2 == 0";
+        final ScriptContext context = runtime.run(expression);
+
+        Assertions.assertTrue(context.errors().isEmpty());
+        Assertions.assertTrue(context.runtimeErrors().isEmpty());
+        Assertions.assertTrue(ValidateExpressionRuntime.valid(context));
+    }
+
+    @Test
+    void testExpressionWithGlobal() {
+        final ValidateExpressionRuntime runtime = this.applicationContext.get(ValidateExpressionRuntime.class);
+        runtime.global("a", 12);
+        final String expression = "a == 12";
+        final ScriptContext context = runtime.run(expression);
+
+        Assertions.assertTrue(context.errors().isEmpty());
+        Assertions.assertTrue(context.runtimeErrors().isEmpty());
+        Assertions.assertTrue(ValidateExpressionRuntime.valid(context));
+    }
+
+    @Test
+    void testExpressionWithGlobalFunctionAccess() {
+        final ValidateExpressionRuntime runtime = this.applicationContext.get(ValidateExpressionRuntime.class);
+        runtime.global("context", this.applicationContext);
+        final String expression = "context != null and context.log() != null";
+        final ScriptContext context = runtime.run(expression);
+
+        Assertions.assertTrue(context.errors().isEmpty());
+        Assertions.assertTrue(context.runtimeErrors().isEmpty());
+        Assertions.assertTrue(ValidateExpressionRuntime.valid(context));
+    }
+
+    @Test
+    void testScriptWithGlobalFunctionAccess() {
+        final StandardRuntime runtime = this.applicationContext.get(StandardRuntime.class);
+        runtime.global("context", this.applicationContext);
+        final String expression = "context.log().info(\"Hello world!\")";
+        final ScriptContext context = runtime.run(expression);
+
+        Assertions.assertTrue(context.errors().isEmpty());
+        Assertions.assertTrue(context.runtimeErrors().isEmpty());
+    }
+
+    @Test
+    void testExpressionWithNativeAccess() {
+        final ValidateExpressionRuntime runtime = this.applicationContext.get(ValidateExpressionRuntime.class);
+        final String expression = "log() != null";
+        runtime.module("application", new InstanceNativeModule(this.applicationContext));
+        final ScriptContext context = runtime.run(expression);
+
+        Assertions.assertTrue(context.errors().isEmpty());
+        Assertions.assertTrue(context.runtimeErrors().isEmpty());
+        Assertions.assertTrue(ValidateExpressionRuntime.valid(context));
+    }
+
+    @Test
+    void testMultilineWithComments() {
+        final StandardRuntime runtime = this.applicationContext.get(StandardRuntime.class);
+        final String expression = """
+                print("Hello world!");
+                
+                # This is a comment
+                print("Hello world 2!");
+                
+                // This is also a comment, print("Hello world 3!");
+                print("Hello world 4!");
+                
+                /* This is a multi-line comment
+                see?!
+                */
+                print("Hello world 5!");
+                """;
+        final ScriptContext context = runtime.run(expression);
+
+        Assertions.assertTrue(context.errors().isEmpty());
+        Assertions.assertTrue(context.runtimeErrors().isEmpty());
+
+        final List<Comment> comments = context.comments();
+        Assertions.assertEquals(3, comments.size());
+
+        // Comments are not trimmed, so we need to include spaces in the expected result
+        Assertions.assertEquals(" This is a comment", comments.get(0).text());
+        Assertions.assertEquals(" This is also a comment, print(\"Hello world 3!\");", comments.get(1).text());
+        Assertions.assertEquals(" This is a multi-line comment\nsee?!\n", comments.get(2).text());
+    }
+
+    @Test
+    void testGlobalResultTracking() {
+        final StandardRuntime runtime = this.applicationContext.get(StandardRuntime.class);
+        final String expression = """
+                var a = 12;
+                var b = 13;
+                var c = a + b;
+                """;
+        final ScriptContext context = runtime.run(expression);
+
+        Assertions.assertTrue(context.errors().isEmpty());
+        Assertions.assertTrue(context.runtimeErrors().isEmpty());
+
+        final Map<String, Object> results = context.interpreter().global();
+        Assertions.assertFalse(results.isEmpty());
+        Assertions.assertEquals(12d, results.get("a"));
+        Assertions.assertEquals(13d, results.get("b"));
+        Assertions.assertEquals(25d, results.get("c"));
+    }
+
+    @ParameterizedTest
+    @MethodSource("phases")
+    void testPhaseCustomizers(final Phase phase) {
+        final StandardRuntime runtime = this.applicationContext.get(StandardRuntime.class);
+        final String expression = "1 == 1";
+
+        final AtomicBoolean called = new AtomicBoolean(false);
+        final CodeCustomizer customizer = new AbstractCodeCustomizer(phase) {
+            @Override
+            public void call(final ScriptContext context) {
+                called.set(true);
+            }
+        };
+        runtime.customizer(customizer);
+
+        runtime.run(expression);
+        Assertions.assertTrue(called.get());
+    }
+}
