864a1d1c781ec77f799ee1d6e0ae82680be18083
Guus Lieben <guus@xendox.com>
2022-07-03 20:00:28 +0200

    #770 Document variable scopes

--- hartshorn-hsl/src/main/java/org/dockbox/hartshorn/hsl/interpreter/VariableScope.java
+++ hartshorn-hsl/src/main/java/org/dockbox/hartshorn/hsl/interpreter/VariableScope.java
@@ -6,6 +6,14 @@ import org.dockbox.hartshorn.hsl.token.Token;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * A variable scope represents all declared variables inside a specific scope, with access to a
+ * potential enclosing scope. An example of a valid scope is inside any code block, such as an
+ * if-statement's body. In this example the enclosing scope is the global scope of the script.
+ *
+ * @author Guus Lieben
+ * @since 22.4
+ */
 public class VariableScope {
 
     private final VariableScope enclosing;
@@ -19,10 +27,24 @@ public class VariableScope {
         this.enclosing = enclosing;
     }
 
+    /**
+     * Gets all variable values declared inside the scope, identified by their name.
+     * @return The variable values.
+     */
     public Map<String, Object> values() {
         return this.valuesMap;
     }
 
+    /**
+     * Gets the variable value for the given identifier, if it exists. If the variable
+     * doesn't exist in this scope, an attempt is made to look it up in the enclosing
+     * scope. If the token doesn't exist in any of the accessible scopes, a
+     * {@link RuntimeError} is thrown.
+     *
+     * @param name The identifier for the variable.
+     * @return The value of the variable.
+     * @throws RuntimeError If the variable is not defined.
+     */
     public Object get(final Token name) {
         if (this.valuesMap.containsKey(name.lexeme())) {
             return this.valuesMap.get(name.lexeme());
@@ -34,10 +56,26 @@ public class VariableScope {
         throw new RuntimeError(name, "Undefined variable '" + name.lexeme() + "'.");
     }
 
+    /**
+     * Defines a new variable and assigns the given value to it. If there is already a
+     * variable with the given name, the value is modified to equal the given value.
+     *
+     * @param name The name of the variable.
+     * @param value The value to assign.
+     */
     public void define(final String name, final Object value) {
         this.valuesMap.put(name, value);
     }
 
+    /**
+     * Reassigns the given value to an existing variable. If the variable does not exist
+     * within this scope, it will be looked up in enclosing scopes. If the variable does
+     * not exist in any enclosing scope, a {@link RuntimeError} is thrown.
+     *
+     * @param name The identifier for the variable.
+     * @param value The value to assign.
+     * @throws RuntimeError If the variable does not exist.
+     */
     public void assign(final Token name, final Object value) {
         if (this.valuesMap.containsKey(name.lexeme())) {
             this.valuesMap.put(name.lexeme(), value);
@@ -54,14 +92,45 @@ public class VariableScope {
     void assignAt(final int distance, final Token name, final Object value) {
         this.ancestor(name, distance).valuesMap.put(name.lexeme(), value);
     }
+
+    /**
+     * Checks if this scope contains a variable with the given name. This does not attempt
+     * to look up the variable in an enclosing scope.
+     *
+     * @param name The name of the variable.
+     * @return {@code true} if the variable exists, or {@code false}.
+     */
     public boolean contains(final String name) {
         return this.valuesMap.containsKey(name);
     }
 
+    /**
+     * Gets the value of the variable with the given name in a scope which is a given amount
+     * of steps up from the current scope. Every step up indicates the enclosing scope of
+     * the previous step is selected. If there is no enclosing scope matching the amount of
+     * steps, a {@link RuntimeError} is thrown.
+     *
+     * @param at The position from where the request is made.
+     * @param distance The amount of steps up.
+     * @param name The name of the variable.
+     * @return The value of the variable.
+     * @throws RuntimeError If there is no enclosing scope matching the amount of steps.
+     */
     public Object getAt(final Token at, final int distance, final String name) {
         return this.ancestor(at, distance).valuesMap.get(name);
     }
 
+    /**
+     * Gets the value of the variable with the given name in a scope which is a given amount
+     * of steps up from the current scope. Every step up indicates the enclosing scope of
+     * the previous step is selected. If there is no enclosing scope matching the amount of
+     * steps, a {@link RuntimeError} is thrown.
+     *
+     * @param name The identifier for the variable
+     * @param distance The amount of steps up.
+     * @return The value of the variable.
+     * @throws RuntimeError If there is no enclosing scope matching the amount of steps.
+     */
     public Object getAt(final Token name, final int distance) {
         return this.getAt(name, distance, name.lexeme());
     }
@@ -77,6 +146,12 @@ public class VariableScope {
         return variableScope;
     }
 
+    /**
+     * Gets the enclosing scope, if it exists. If no existing scope exists, {@code null}
+     * is returned.
+     *
+     * @return The enclosing {@link VariableScope}, or {@code null}.
+     */
     public VariableScope enclosing() {
         return this.enclosing;
     }
