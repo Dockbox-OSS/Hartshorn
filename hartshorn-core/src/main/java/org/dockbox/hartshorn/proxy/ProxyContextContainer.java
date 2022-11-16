13c59e56af0df6ff8b8d8982866526688faf58ae
Guus Lieben <guus@xendox.com>
2022-11-16 20:17:16 +0100

    #873 Track context addition as factory modification

--- hartshorn-core/src/main/java/org/dockbox/hartshorn/proxy/ProxyContextContainer.java
+++ hartshorn-core/src/main/java/org/dockbox/hartshorn/proxy/ProxyContextContainer.java
@@ -18,11 +18,18 @@ package org.dockbox.hartshorn.proxy;
 
 import org.dockbox.hartshorn.context.Context;
 import org.dockbox.hartshorn.context.DefaultContext;
+import org.dockbox.hartshorn.context.NamedContext;
 import org.dockbox.hartshorn.util.collections.MultiMap;
 
 import java.util.Set;
 
 public class ProxyContextContainer extends DefaultContext {
+
+    private final Runnable onModify;
+
+    public ProxyContextContainer(final Runnable onModify) {
+        this.onModify = onModify;
+    }
 
     public Set<Context> contexts() {
         return super.contexts;
@@ -30,5 +37,23 @@ public class ProxyContextContainer extends DefaultContext {
 
     public MultiMap<String, Context> namedContexts() {
         return super.namedContexts;
+    }
+
+    @Override
+    public <C extends Context> void add(final C context) {
+        super.add(context);
+        this.onModify.run();
+    }
+
+    @Override
+    public <N extends NamedContext> void add(final N context) {
+        super.add(context);
+        this.onModify.run();
+    }
+
+    @Override
+    public <C extends Context> void add(final String name, final C context) {
+        super.add(name, context);
+        this.onModify.run();
     }
 }
