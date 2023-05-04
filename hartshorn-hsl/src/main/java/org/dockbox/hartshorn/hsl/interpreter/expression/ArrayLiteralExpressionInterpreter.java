172da1010ddee8173fe058594f63eb768f9688e8
Guus Lieben <guus@xendox.com>
2023-05-04 19:45:18 +0200

    #930 Add tests for array literals

--- hartshorn-hsl/src/main/java/org/dockbox/hartshorn/hsl/interpreter/expression/ArrayLiteralExpressionInterpreter.java
+++ hartshorn-hsl/src/main/java/org/dockbox/hartshorn/hsl/interpreter/expression/ArrayLiteralExpressionInterpreter.java
@@ -18,22 +18,22 @@ package org.dockbox.hartshorn.hsl.interpreter.expression;
 
 import org.dockbox.hartshorn.hsl.ast.expression.ArrayLiteralExpression;
 import org.dockbox.hartshorn.hsl.ast.expression.Expression;
+import org.dockbox.hartshorn.hsl.interpreter.ASTNodeInterpreter;
 import org.dockbox.hartshorn.hsl.interpreter.Array;
 import org.dockbox.hartshorn.hsl.interpreter.InterpreterAdapter;
-import org.dockbox.hartshorn.hsl.interpreter.ASTNodeInterpreter;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class ArrayLiteralExpressionInterpreter implements ASTNodeInterpreter<Object, ArrayLiteralExpression> {
 
     @Override
     public Object interpret(final ArrayLiteralExpression node, final InterpreterAdapter adapter) {
-        final List<Object> values = new ArrayList<>();
-        for(final Expression expression : node.elements()) {
-            final Object evaluate = adapter.evaluate(expression);
-            values.add(evaluate);
+        final Object[] values = new Object[node.elements().size()];
+        final List<Expression> elements = node.elements();
+        for (int i = 0, elementsSize = elements.size(); i < elementsSize; i++) {
+            final Expression expression = elements.get(i);
+            values[i] = adapter.evaluate(expression);
         }
-        return new Array(values.toArray());
+        return new Array(values);
     }
 }
--- hartshorn-hsl/src/test/java/test/org/dockbox/hartshorn/hsl/interpreter/expression/ArrayLiteralExpressionInterpreterTests.java
+++ hartshorn-hsl/src/test/java/test/org/dockbox/hartshorn/hsl/interpreter/expression/ArrayLiteralExpressionInterpreterTests.java
@@ -16,4 +16,63 @@
 
 package test.org.dockbox.hartshorn.hsl.interpreter.expression;
 
-public class ArrayLiteralExpressionInterpreterTests {}
+import org.dockbox.hartshorn.hsl.ast.expression.ArrayLiteralExpression;
+import org.dockbox.hartshorn.hsl.ast.expression.Expression;
+import org.dockbox.hartshorn.hsl.ast.expression.LiteralExpression;
+import org.dockbox.hartshorn.hsl.interpreter.ASTNodeInterpreter;
+import org.dockbox.hartshorn.hsl.interpreter.Array;
+import org.dockbox.hartshorn.hsl.interpreter.CacheOnlyResultCollector;
+import org.dockbox.hartshorn.hsl.interpreter.Interpreter;
+import org.dockbox.hartshorn.hsl.interpreter.InterpreterAdapter;
+import org.dockbox.hartshorn.hsl.interpreter.ResultCollector;
+import org.dockbox.hartshorn.hsl.interpreter.expression.ArrayLiteralExpressionInterpreter;
+import org.dockbox.hartshorn.hsl.token.Token;
+import org.dockbox.hartshorn.hsl.token.TokenType;
+import org.jetbrains.annotations.NotNull;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Map;
+
+public class ArrayLiteralExpressionInterpreterTests {
+
+    @Test
+    void testEmptyArrayLiteralYieldsEmptyArrayObject() {
+        final ArrayLiteralExpression expression = createExpression(List.of());
+
+        final ASTNodeInterpreter<Object, ArrayLiteralExpression> interpreter = new ArrayLiteralExpressionInterpreter();
+        final Object interpreted = interpreter.interpret(expression, null);
+        Assertions.assertNotNull(interpreted);
+        Assertions.assertTrue(interpreted instanceof Array);
+
+        final Array array = (Array) interpreted;
+        Assertions.assertEquals(0, array.length());
+    }
+
+    @Test
+    void testArrayLiteralYieldsArrayObject() {
+        final Token value = Token.of(TokenType.STRING).literal("test").build();
+        final LiteralExpression literalExpression = new LiteralExpression(value, value.literal());
+        final ArrayLiteralExpression expression = createExpression(List.of(literalExpression));
+
+        final ResultCollector resultCollector = new CacheOnlyResultCollector();
+        final InterpreterAdapter interpreter = new Interpreter(resultCollector, Map.of(), null);
+
+        final ASTNodeInterpreter<Object, ArrayLiteralExpression> expressionInterpreter = new ArrayLiteralExpressionInterpreter();
+        final Object interpreted = expressionInterpreter.interpret(expression, interpreter);
+        Assertions.assertNotNull(interpreted);
+        Assertions.assertTrue(interpreted instanceof Array);
+
+        final Array array = (Array) interpreted;
+        Assertions.assertEquals(1, array.length());
+        Assertions.assertEquals("test", array.value(0));
+    }
+
+    @NotNull
+    private static ArrayLiteralExpression createExpression(final List<Expression> expressions) {
+        final Token open = Token.of(TokenType.ARRAY_OPEN).lexeme("[").build();
+        final Token close = Token.of(TokenType.ARRAY_CLOSE).lexeme("]").build();
+        return new ArrayLiteralExpression(open, close, expressions);
+    }
+}
