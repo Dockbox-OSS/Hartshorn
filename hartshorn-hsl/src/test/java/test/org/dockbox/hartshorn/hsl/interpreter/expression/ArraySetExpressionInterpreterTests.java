82a1e8698491f1ba5d9093fe4bf55b1f4d1916d8
Guus Lieben <guus@xendox.com>
2023-05-05 19:06:03 +0200

    #930 Tests for ArraySetExpressionInterpreter

--- hartshorn-hsl/src/test/java/test/org/dockbox/hartshorn/hsl/interpreter/expression/ArraySetExpressionInterpreterTests.java
+++ hartshorn-hsl/src/test/java/test/org/dockbox/hartshorn/hsl/interpreter/expression/ArraySetExpressionInterpreterTests.java
@@ -16,4 +16,71 @@
 
 package test.org.dockbox.hartshorn.hsl.interpreter.expression;
 
-public class ArraySetExpressionInterpreterTests {}
+import org.dockbox.hartshorn.hsl.ast.expression.ArraySetExpression;
+import org.dockbox.hartshorn.hsl.ast.expression.LiteralExpression;
+import org.dockbox.hartshorn.hsl.interpreter.ASTNodeInterpreter;
+import org.dockbox.hartshorn.hsl.interpreter.Array;
+import org.dockbox.hartshorn.hsl.interpreter.CacheOnlyResultCollector;
+import org.dockbox.hartshorn.hsl.interpreter.Interpreter;
+import org.dockbox.hartshorn.hsl.interpreter.InterpreterAdapter;
+import org.dockbox.hartshorn.hsl.interpreter.ResultCollector;
+import org.dockbox.hartshorn.hsl.interpreter.expression.ArraySetExpressionInterpreter;
+import org.dockbox.hartshorn.hsl.token.Token;
+import org.dockbox.hartshorn.hsl.token.TokenType;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Map;
+
+public class ArraySetExpressionInterpreterTests {
+
+    @Test
+    void testSetWithinArrayRange() {
+        final ResultCollector resultCollector = new CacheOnlyResultCollector();
+        final InterpreterAdapter interpreter = new Interpreter(resultCollector, Map.of(), null);
+
+        final Object[] realArray = { "test" };
+        final int targetIndex = 0;
+
+        final Token indexToken = Token.of(TokenType.NUMBER).literal(targetIndex).build();
+        final LiteralExpression index = new LiteralExpression(indexToken, targetIndex);
+
+        final Token arrayIdentifier = Token.of(TokenType.IDENTIFIER).lexeme("test").build();
+        final Array hslArray = new Array(realArray);
+        interpreter.visitingScope().define(arrayIdentifier.lexeme(), hslArray);
+
+        final Token valueToken = Token.of(TokenType.STRING).literal("value").build();
+        final LiteralExpression literalExpression = new LiteralExpression(valueToken, "value");
+
+        final ASTNodeInterpreter<Object, ArraySetExpression> expressionInterpreter = new ArraySetExpressionInterpreter();
+        final ArraySetExpression setExpression = new ArraySetExpression(arrayIdentifier, index, literalExpression);
+
+        final Object interpreted = expressionInterpreter.interpret(setExpression, interpreter);
+        Assertions.assertEquals(literalExpression.value(), interpreted);
+        Assertions.assertEquals(literalExpression.value(), hslArray.value(targetIndex));
+    }
+
+    @Test
+    void testSetOutsideRangeThrowsOutOfBounds() {
+        final ResultCollector resultCollector = new CacheOnlyResultCollector();
+        final InterpreterAdapter interpreter = new Interpreter(resultCollector, Map.of(), null);
+
+        final Object[] realArray = { "test" };
+        final int targetIndex = 1;
+
+        final Token indexToken = Token.of(TokenType.NUMBER).literal(targetIndex).build();
+        final LiteralExpression index = new LiteralExpression(indexToken, targetIndex);
+
+        final Token arrayIdentifier = Token.of(TokenType.IDENTIFIER).lexeme("test").build();
+        final Array hslArray = new Array(realArray);
+        interpreter.visitingScope().define(arrayIdentifier.lexeme(), hslArray);
+
+        final Token valueToken = Token.of(TokenType.STRING).literal("value").build();
+        final LiteralExpression literalExpression = new LiteralExpression(valueToken, "value");
+
+        final ASTNodeInterpreter<Object, ArraySetExpression> expressionInterpreter = new ArraySetExpressionInterpreter();
+        final ArraySetExpression setExpression = new ArraySetExpression(arrayIdentifier, index, literalExpression);
+
+        Assertions.assertThrows(IndexOutOfBoundsException.class, () -> expressionInterpreter.interpret(setExpression, interpreter));
+    }
+}
