3bade2204fd0f8f5ebb2969d2bb978c27639f68c
Guus Lieben <guus@xendox.com>
2023-06-16 20:36:18 +0200

    #973 Correctly install scope in auto configuration DependencyContext

--- hartshorn-core/src/main/java/org/dockbox/hartshorn/inject/AutoConfiguringDependencyContext.java
+++ hartshorn-core/src/main/java/org/dockbox/hartshorn/inject/AutoConfiguringDependencyContext.java
@@ -62,14 +62,14 @@ public class AutoConfiguringDependencyContext<T> implements DependencyContext<T>
     @Override
     public void configure(final BindingFunction<T> function) throws ComponentConfigurationException {
         final InstanceType instanceType = this.instanceType();
-        final BindingFunction<T> installTo = function
-                .priority(this.priority)
-                .installTo(this.scope);
+        function.priority(this.priority);
+        if (this.scope != Scope.DEFAULT_SCOPE.installableScopeType()) function.installTo(this.scope);
+
         try {
             switch (instanceType) {
-                case SUPPLIER -> installTo.to(this.supplier);
-                case SINGLETON -> installTo.singleton(this.supplier.get());
-                case LAZY_SINGLETON -> installTo.lazySingleton(this.supplier);
+                case SUPPLIER -> function.to(this.supplier);
+                case SINGLETON -> function.singleton(this.supplier.get());
+                case LAZY_SINGLETON -> function.lazySingleton(this.supplier);
             }
         }
         catch (final ApplicationException e) {
