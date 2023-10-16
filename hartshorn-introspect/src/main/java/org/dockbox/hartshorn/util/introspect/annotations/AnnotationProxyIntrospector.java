13c9d023c9946e70c510c27772b2c259e470cbb7
Guus Lieben <guus@xendox.com>
2023-10-16 19:39:09 +0200

    #994 Update license headers

--- hartshorn-introspect/src/main/java/org/dockbox/hartshorn/util/introspect/annotations/AnnotationProxyIntrospector.java
+++ hartshorn-introspect/src/main/java/org/dockbox/hartshorn/util/introspect/annotations/AnnotationProxyIntrospector.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2019-2023 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.dockbox.hartshorn.util.introspect.annotations;
 
 import java.lang.annotation.Annotation;
@@ -16,17 +32,17 @@ public class AnnotationProxyIntrospector<T extends Annotation> implements ProxyI
 
     @Override
     public Class<T> targetClass() {
-        return TypeUtils.adjustWildcards(annotation.annotationType(), Class.class);
+        return TypeUtils.adjustWildcards(this.annotation.annotationType(), Class.class);
     }
 
     @Override
     public Class<T> proxyClass() {
-        return TypeUtils.adjustWildcards(annotation.getClass(), Class.class);
+        return TypeUtils.adjustWildcards(this.annotation.getClass(), Class.class);
     }
 
     @Override
     public T proxy() {
-        return annotation;
+        return this.annotation;
     }
 
     @Override
