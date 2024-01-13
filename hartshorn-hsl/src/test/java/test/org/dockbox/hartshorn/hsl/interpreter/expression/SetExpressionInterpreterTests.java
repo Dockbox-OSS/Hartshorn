d578fa281a66f2fa99e8ee1ad5418afc91e35f8b
Guus Lieben <guus@xendox.com>
2024-01-13 19:09:13 +0100

    #1057 Initial SetExpression tests

--- hartshorn-hsl/src/test/java/test/org/dockbox/hartshorn/hsl/interpreter/expression/SetExpressionInterpreterTests.java
+++ hartshorn-hsl/src/test/java/test/org/dockbox/hartshorn/hsl/interpreter/expression/SetExpressionInterpreterTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019-2023 the original author or authors.
+ * Copyright 2019-2024 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,4 +16,55 @@
 
 package test.org.dockbox.hartshorn.hsl.interpreter.expression;
 
-public class SetExpressionInterpreterTests {}
+import org.dockbox.hartshorn.hsl.ast.expression.LiteralExpression;
+import org.dockbox.hartshorn.hsl.ast.expression.SetExpression;
+import org.dockbox.hartshorn.hsl.ast.expression.VariableExpression;
+import org.dockbox.hartshorn.hsl.interpreter.InterpreterAdapter;
+import org.dockbox.hartshorn.hsl.interpreter.expression.SetExpressionInterpreter;
+import org.dockbox.hartshorn.hsl.objects.external.ExternalInstance;
+import org.dockbox.hartshorn.hsl.token.Token;
+import org.dockbox.hartshorn.hsl.token.TokenType;
+import org.dockbox.hartshorn.util.introspect.NativeProxyLookup;
+import org.dockbox.hartshorn.util.introspect.annotations.VirtualHierarchyAnnotationLookup;
+import org.dockbox.hartshorn.util.introspect.reflect.ReflectionIntrospector;
+import org.dockbox.hartshorn.util.introspect.view.TypeView;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import test.org.dockbox.hartshorn.hsl.interpreter.InterpreterTestHelper;
+
+public class SetExpressionInterpreterTests {
+
+    public static class TestClass {
+        public String value;
+    }
+
+    @Test
+    void testSetExpressionCanSetIfDefined() {
+        InterpreterAdapter adapter = InterpreterTestHelper.createInterpreterAdapter();
+        TestClass reference = new TestClass();
+
+        ReflectionIntrospector introspector = new ReflectionIntrospector(new NativeProxyLookup(),
+                new VirtualHierarchyAnnotationLookup());
+        TypeView<TestClass> typeView = introspector.introspect(TestClass.class);
+
+        ExternalInstance externalInstance = new ExternalInstance(reference, typeView);
+        adapter.visitingScope().define("reference", externalInstance);
+
+        Token externalName = Token.of(TokenType.IDENTIFIER).lexeme("reference").build();
+        VariableExpression variableExpression = new VariableExpression(externalName);
+
+        Token propertyValue = Token.of(TokenType.STRING).literal("test").build();
+        LiteralExpression literalValue = new LiteralExpression(propertyValue, propertyValue.literal());
+
+        Token propertyName = Token.of(TokenType.IDENTIFIER).lexeme("value").build();
+        SetExpression setExpression = new SetExpression(variableExpression, propertyName, literalValue);
+        SetExpressionInterpreter interpreter = new SetExpressionInterpreter();
+
+        Object interpretedValue = interpreter.interpret(setExpression, adapter);
+        Assertions.assertEquals(literalValue.value(), interpretedValue);
+
+        Object value = reference.value;
+        Assertions.assertEquals(literalValue.value(), value);
+    }
+}
