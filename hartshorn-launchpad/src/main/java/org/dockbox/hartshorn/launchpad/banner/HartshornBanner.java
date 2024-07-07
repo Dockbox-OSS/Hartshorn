8ac3b71a05e9b1cda9143779fc1fee984cdf5aa1
Guus Lieben <guus@xendox.com>
2024-07-07 16:04:18 +0200

    #1101 Extract environment and application context elements to Launchpad

---
+++ hartshorn-launchpad/src/main/java/org/dockbox/hartshorn/launchpad/banner/HartshornBanner.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2019-2024 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.dockbox.hartshorn.launchpad.banner;
+
+import java.util.List;
+
+import org.dockbox.hartshorn.application.Hartshorn;
+import org.dockbox.hartshorn.launchpad.logging.AnsiColor;
+import org.dockbox.hartshorn.launchpad.logging.AnsiMessage;
+import org.dockbox.hartshorn.launchpad.logging.AnsiStyle;
+
+/**
+ * The default banner of Hartshorn. This banner is printed when the application starts if
+ * no custom banner is provided.
+ *
+ * @since 0.4.13
+ *
+ * @author Guus Lieben
+ */
+public class HartshornBanner extends AbstractConsoleBanner {
+
+    private static final List<String> BANNER_LINES = List.of(
+            "",
+            "     _   _            _       _",
+            "    | | | | __ _ _ __| |_ ___| |__   ___  _ __ _ __",
+            "    | |_| |/ _` | '__| __/ __| '_ \\ / _ \\| '__| '_ \\",
+            "    |  _  | (_| | |  | |_\\__ \\ | | | (_) | |  | | | |",
+            AnsiMessage.of("====", AnsiColor.CYAN, AnsiStyle.BOLD)
+                    .append("|_| |_|\\__,_|_|")
+                    .append(AnsiMessage.of("===", AnsiColor.CYAN, AnsiStyle.BOLD))
+                    .append("\\__|___/_|")
+                    .append(AnsiMessage.of("=", AnsiColor.CYAN, AnsiStyle.BOLD))
+                    .append("|_|\\___/|_|")
+                    .append(AnsiMessage.of("==", AnsiColor.CYAN, AnsiStyle.BOLD))
+                    .append("|_|")
+                    .append(AnsiMessage.of("=", AnsiColor.CYAN, AnsiStyle.BOLD))
+                    .append("|_|")
+                    .append(AnsiMessage.of("====", AnsiColor.CYAN, AnsiStyle.BOLD))
+                    .toString(),
+            AnsiMessage.of("                                  ")
+                    .append(AnsiMessage.of("-< ", AnsiColor.WHITE))
+                    .append(AnsiMessage.of("Hartshorn v%s".formatted(Hartshorn.VERSION), AnsiColor.GREEN))
+                    .append(AnsiMessage.of(" >-\n", AnsiColor.WHITE))
+                    .toString()
+    );
+
+    @Override
+    protected Iterable<String> lines() {
+        return BANNER_LINES;
+    }
+}
---
+++ hartshorn-launchpad/src/main/java/org/dockbox/hartshorn/launchpad/logging/logback/LogbackLevelConverter.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2019-2024 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.dockbox.hartshorn.launchpad.logging.logback;
+
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.core.pattern.color.ANSIConstants;
+import ch.qos.logback.core.pattern.color.ForegroundCompositeConverterBase;
+
+/**
+ * A logback converter that converts the log level to a color. This affects output for
+ * any known log level. This is mostly for 'at a glance' readability of log lines in a
+ * console.
+ *
+ * @since 0.6.0
+ *
+ * @author Guus Lieben
+ */
+public class LogbackLevelConverter extends ForegroundCompositeConverterBase<ILoggingEvent> {
+
+    @Override
+    protected String getForegroundColorCode(ILoggingEvent event) {
+        final Level level = event.getLevel();
+        return switch (level.toInt()) {
+            case Level.ERROR_INT -> ANSIConstants.RED_FG;
+            case Level.WARN_INT -> ANSIConstants.YELLOW_FG;
+            case Level.INFO_INT -> ANSIConstants.MAGENTA_FG;
+            case Level.DEBUG_INT -> ANSIConstants.CYAN_FG;
+            case Level.TRACE_INT -> ANSIConstants.WHITE_FG;
+            default -> ANSIConstants.DEFAULT_FG;
+        };
+    }
+}
