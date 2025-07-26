b256c571f627c9b6d5cc2017d0b9601e3122ed4e
Guus Lieben <guus@xendox.com>
2025-07-26 13:41:54 +0200

    #1125 Move Kotlin extensions to dedicated project

--- hartshorn-launchpad-kt-extensions/src/main/kotlin/application/HartshornApplicationExtensions.kt
+++ hartshorn-launchpad-kt-extensions/src/main/kotlin/application/HartshornApplicationExtensions.kt
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.dockbox.hartshorn.application
+package application
 
 import org.dockbox.hartshorn.launchpad.HartshornApplication.ApplicationBootstrap
 import org.dockbox.hartshorn.launchpad.ApplicationContext
