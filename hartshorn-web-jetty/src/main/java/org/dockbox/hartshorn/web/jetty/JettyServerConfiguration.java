14cb3705b16ba33291e9a4fe3830802459da960d
Guus Lieben <guus@xendox.com>
2026-01-24 19:22:02 +0100

    #1161 Add property configurations for Jetty server components

--- hartshorn-web-jetty/src/main/java/org/dockbox/hartshorn/web/jetty/JettyServerConfiguration.java
+++ hartshorn-web-jetty/src/main/java/org/dockbox/hartshorn/web/jetty/JettyServerConfiguration.java
@@ -1,18 +1,29 @@
 package org.dockbox.hartshorn.web.jetty;
 
 import org.dockbox.hartshorn.inject.annotations.Fuzzy;
+import org.dockbox.hartshorn.inject.annotations.Named;
 import org.dockbox.hartshorn.inject.annotations.PropertyValue;
 import org.dockbox.hartshorn.inject.annotations.configuration.Configuration;
 import org.dockbox.hartshorn.inject.annotations.configuration.Prototype;
 import org.dockbox.hartshorn.inject.annotations.configuration.Singleton;
 import org.dockbox.hartshorn.inject.collection.ComponentCollection;
+import org.dockbox.hartshorn.inject.condition.support.RequiresAbsentBinding;
+import org.dockbox.hartshorn.inject.condition.support.RequiresProperty;
+import org.dockbox.hartshorn.inject.provider.ComponentProvider;
 import org.dockbox.hartshorn.launchpad.condition.RequiresActivator;
 import org.dockbox.hartshorn.util.configure.Customizer;
-import org.dockbox.hartshorn.web.chain.RequestHandlerChain;
 import org.dockbox.hartshorn.web.UseWebServer;
 import org.dockbox.hartshorn.web.WebServer;
+import org.dockbox.hartshorn.web.chain.RequestFilterChain;
 import org.dockbox.hartshorn.web.jetty.route.JettyRequestHandler;
+import org.eclipse.jetty.io.ArrayByteBufferPool;
+import org.eclipse.jetty.io.ByteBufferPool;
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.ServerConnector;
+import org.eclipse.jetty.util.thread.QueuedThreadPool;
+import org.eclipse.jetty.util.thread.ScheduledExecutorScheduler;
+import org.eclipse.jetty.util.thread.Scheduler;
+import org.eclipse.jetty.util.thread.ThreadPool;
 
 @Configuration
 @RequiresActivator(UseWebServer.class)
@@ -25,13 +36,73 @@ public class JettyServerConfiguration {
 
     @Prototype
     public Server jettyServer(
-            RequestHandlerChain chain,
+            RequestFilterChain chain,
+            ThreadPool threadPool,
+            Scheduler scheduler,
+            ByteBufferPool bufferPool,
             @Fuzzy ComponentCollection<Customizer<Server>> customizers,
             @PropertyValue(name = "hartshorn.web.port", defaultValue = "8080") int port
     ) {
-        Server server = new Server(port);
+        Server server = new Server(threadPool, scheduler, bufferPool);
         server.setHandler(new JettyRequestHandler(chain));
+
+        ServerConnector connector = new ServerConnector(server);
+        connector.setPort(port);
+        server.addConnector(connector);
+
         customizers.forEach(customizer -> customizer.configure(server));
+
         return server;
+    }
+
+    @Prototype
+    @RequiresAbsentBinding(value = ThreadPool.class, name = "jettyServerThreadPool")
+    public ThreadPool jettyServerThreadPool() {
+        return new QueuedThreadPool();
+    }
+
+    @Prototype
+    @Named("jettyServerThreadPool")
+    @RequiresProperty(name = "hartshorn.web.jetty.threadpool")
+    public ThreadPool jettyServerCustomThreadPool(
+            ComponentProvider componentProvider,
+            @PropertyValue(name = "hartshorn.web.jetty.threadpool")
+            Class<? extends ThreadPool> threadPool
+    ) {
+        return componentProvider.get(threadPool);
+    }
+
+    @Prototype
+    @RequiresAbsentBinding(value = Scheduler.class, name = "jettyServerScheduler")
+    public Scheduler jettyServerScheduler() {
+        return new ScheduledExecutorScheduler();
+    }
+
+    @Prototype
+    @Named("jettyServerScheduler")
+    @RequiresProperty(name = "hartshorn.web.jetty.scheduler")
+    public Scheduler jettyServerCustomScheduler(
+            ComponentProvider componentProvider,
+            @PropertyValue(name = "hartshorn.web.jetty.scheduler")
+            Class<? extends Scheduler> scheduler
+    ) {
+        return componentProvider.get(scheduler);
+    }
+
+    @Prototype
+    @RequiresAbsentBinding(value = ByteBufferPool.class, name = "jettyServerBufferPool")
+    public ByteBufferPool jettyServerBufferPool() {
+        return new ArrayByteBufferPool();
+    }
+
+    @Prototype
+    @Named("jettyServerBufferPool")
+    @RequiresProperty(name = "hartshorn.web.jetty.bufferpool")
+    public ByteBufferPool jettyServerCustomBufferPool(
+            ComponentProvider componentProvider,
+            @PropertyValue(name = "hartshorn.web.jetty.bufferpool")
+            Class<? extends ByteBufferPool> bufferPool
+    ) {
+        return componentProvider.get(bufferPool);
     }
 }
--- hartshorn-web-jetty/src/main/java/org/dockbox/hartshorn/web/jetty/JettyWebServer.java
+++ hartshorn-web-jetty/src/main/java/org/dockbox/hartshorn/web/jetty/JettyWebServer.java
@@ -1,10 +1,12 @@
 package org.dockbox.hartshorn.web.jetty;
 
+import org.dockbox.hartshorn.util.stream.CollectorUtilities;
 import org.dockbox.hartshorn.web.ServerException;
 import org.dockbox.hartshorn.web.WebServer;
-import org.eclipse.jetty.server.Connector;
 import org.eclipse.jetty.server.NetworkConnector;
 import org.eclipse.jetty.server.Server;
+
+import java.util.Arrays;
 
 public class JettyWebServer implements WebServer {
 
@@ -12,6 +14,10 @@ public class JettyWebServer implements WebServer {
 
     public JettyWebServer(Server jettyServer) {
         this.jettyServer = jettyServer;
+    }
+
+    public Server jettyServer() {
+        return jettyServer;
     }
 
     @Override
@@ -47,20 +53,13 @@ public class JettyWebServer implements WebServer {
 
     @Override
     public int port() {
-        Connector[] connectors = jettyServer.getConnectors();
-        if (connectors.length == 0) {
-            throw new IllegalStateException("No connectors available to determine port");
-        }
-        if (connectors.length > 1) {
-            throw new IllegalStateException(
-                    "Multiple connectors available, unable to determine port unambiguously"
-            );
-        }
-        for (Connector connector : connectors) {
-            if (connector instanceof NetworkConnector networkConnector) {
-                return networkConnector.getLocalPort();
-            }
-        }
-        throw new IllegalStateException("No network connector available to determine port");
+        return Arrays.stream(jettyServer.getConnectors())
+                .filter(NetworkConnector.class::isInstance)
+                .map(NetworkConnector.class::cast)
+                .collect(CollectorUtilities.toOption())
+                .map(NetworkConnector::getLocalPort)
+                .orElseThrow(() -> new IllegalStateException(
+                        "No network connector available to determine port"
+                ));
     }
 }
