698bdc7b455958eba338843ffc130685c7a5d7ae
Guus Lieben <guus@xendox.com>
2026-01-29 08:37:00 +0100

    #1161 Fix HSL failure in script due to API change

--- hartshorn-hsl/src/test/java/test/org/dockbox/hartshorn/hsl/ScriptRuntimeTests.java
+++ hartshorn-hsl/src/test/java/test/org/dockbox/hartshorn/hsl/ScriptRuntimeTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019-2025 the original author or authors.
+ * Copyright 2019-2026 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -121,7 +121,7 @@ public class ScriptRuntimeTests {
 
     @Test
     void scriptWithGlobalFunctionAccess() {
-        String expression = "context.environment().isBatchMode()";
+        String expression = "context.environment().configuration().isBatchMode()";
         ExecutableScript script = ExecutableScript.of(this.applicationContext, expression);
         script.runtime().global("context", this.applicationContext);
         this.assertNoErrorsReported(script);
