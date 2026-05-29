1e5b5d0ee98ded9351db20a647a0fc3ab48f51da
Guus Lieben <guus@xendox.com>
2026-05-29 11:14:29 +0200

    #1167 Allow specifying parent scope inclusion in declarative injections

--- hartshorn-inject/src/main/java/org/dockbox/hartshorn/inject/annotations/Fuzzy.java
+++ hartshorn-inject/src/main/java/org/dockbox/hartshorn/inject/annotations/Fuzzy.java
@@ -16,12 +16,12 @@
 
 package org.dockbox.hartshorn.inject.annotations;
 
-import org.dockbox.hartshorn.util.introspect.annotations.Extends;
-
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
+import org.dockbox.hartshorn.util.Tristate;
+import org.dockbox.hartshorn.util.introspect.annotations.Extends;
 
 /**
  * Indicates that an injection lookup should be fuzzy, meaning that it should return a value if it
@@ -40,4 +40,12 @@ import java.lang.annotation.Target;
 @Extends(Strict.class)
 @Strict(false)
 public @interface Fuzzy {
+
+    /**
+     * @see Strict#includeParentScope()
+     *
+     * @return whether parent scopes should be included when resolving components for the annotated
+     * key. If {@code UNDEFINED}, the global configuration is used.
+     */
+    Tristate includeParentScope() default Tristate.UNDEFINED;
 }
