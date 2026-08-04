a79e249195f61f422a260b55593d5418fc07edc7
Guus Lieben <guus@xendox.com>
2026-08-04 15:50:29 +0200

    #0 Update web sample to include custom error handler and static resource serving

--- hartshorn-launchpad-samples/application-web/src/main/java/org/dockbox/sample/web/SampleWebApplication.java
+++ hartshorn-launchpad-samples/application-web/src/main/java/org/dockbox/sample/web/SampleWebApplication.java
@@ -1,8 +1,13 @@
 package org.dockbox.sample.web;
 
+import org.dockbox.hartshorn.inject.annotations.CompositeMember;
+import org.dockbox.hartshorn.inject.annotations.configuration.Singleton;
 import org.dockbox.hartshorn.launchpad.HartshornApplication;
 import org.dockbox.hartshorn.reporting.UseReporting;
+import org.dockbox.hartshorn.util.configure.Customizer;
+import org.dockbox.hartshorn.web.HttpStatus;
 import org.dockbox.hartshorn.web.UseWebServer;
+import org.dockbox.hartshorn.web.error.ErrorHandlerRegistry;
 
 @UseWebServer
 @UseReporting
@@ -12,5 +17,16 @@ public class SampleWebApplication {
         HartshornApplication.createApplication(args).initialize(application -> {
             application.includeBasePackages(false);
         });
+    }
+
+    @Singleton
+    @CompositeMember
+    public Customizer<ErrorHandlerRegistry> customErrorHandlers() {
+        return registry -> {
+            registry.register(ArrayIndexOutOfBoundsException.class, (e, _, res) -> {
+                res.setStatus(HttpStatus.OK.code());
+                res.getWriter().write("Manually captured " + e.getMessage());
+            });
+        };
     }
 }
